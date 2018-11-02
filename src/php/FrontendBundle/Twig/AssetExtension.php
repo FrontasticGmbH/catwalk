faa04b98ae1f3c7e1b43599a6f973db7a34ea108
Manuel Pichler <github@manuel-pichler.de>
2018-11-02 09:48:32 +0100

    Added: Custom twig extension that will replace the asset hash

---
+++ src/php/FrontendBundle/Twig/AssetExtension.php
@@ -0,0 +1,39 @@
+<?php
+namespace Frontastic\Catwalk\FrontendBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AssetExtension extends AbstractExtension
+{
+    private $assetHash;
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('asset_hash', [$this, 'updateHash']),
+        ];
+    }
+
+    public function updateHash(string $path)
+    {
+        if ($hash = $this->loadAssetHash()) {
+            return preg_replace('(\.[0-9a-f]+\.(js|css)$)', ".{$hash}.\\1", $path);
+        }
+        return $path;
+    }
+
+    private function loadAssetHash()
+    {
+        if (false === is_null($this->assetHash)) {
+            return $this->assetHash;
+        }
+
+        $file = $_SERVER['DOCUMENT_ROOT'] . '/.fake_preview_hash';
+
+        if (false === file_exists($file)) {
+            return ($this->assetHash = false);
+        }
+        return ($this->assetHash = trim(file_get_contents($file)));
+    }
+}
