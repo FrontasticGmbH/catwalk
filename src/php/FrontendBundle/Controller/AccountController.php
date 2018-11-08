ad686792c72308251dfebff9f5469029a975b555
Kore Nordmann <mail@kore-nordmann.de>
2018-11-08 09:59:47 +0100

    Added routes and basic controller for my account
    (project merge back 2019-01-08)

---
+++ src/php/FrontendBundle/Controller/AccountController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Frontastic\Catwalk\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Frontastic\Catwalk\FrontendBundle\Domain\Node;
+use Frontastic\Catwalk\FrontendBundle\Domain\Page;
+use Frontastic\Catwalk\FrontendBundle\Domain\PageMatcher\PageMatcherContext;
+use Frontastic\Catwalk\ApiCoreBundle\Domain\Context;
+
+class AccountController extends Controller
+{
+    public function indexAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account' => true]));
+    }
+
+    public function profileAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account_profile' => true]));
+    }
+
+    public function addressesAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account_addresses' => true]));
+    }
+
+    public function ordersAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account_orders' => true]));
+    }
+
+    public function wishlistsAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account_wishlists' => true]));
+    }
+
+    public function vouchersAction(Context $context): array
+    {
+        // @TODO: Add information about cart here, so that we can build selectors on top if this?
+        return $this->getNode($context, new PageMatcherContext(['account_vouchers' => true]));
+    }
+
+    private function getNode(Context $context, PageMatcherContext $pageMatcherContext): array
+    {
+        $masterService = $this->get('Frontastic\Catwalk\FrontendBundle\Domain\MasterService');
+        $nodeService = $this->get('Frontastic\Catwalk\FrontendBundle\Domain\NodeService');
+        $dataService = $this->get('Frontastic\Catwalk\FrontendBundle\Domain\ViewDataProvider');
+        $pageService = $this->get('Frontastic\Catwalk\FrontendBundle\Domain\PageService');
+
+        $node = $nodeService->get(
+            $masterService->matchNodeId($pageMatcherContext)
+        );
+        $page = $pageService->fetchForNode($node);
+
+        return [
+            'node' => $node,
+            'page' => $page,
+            'data' => $dataService->fetchDataFor($node, $context, [], $page),
+        ];
+    }
+}
