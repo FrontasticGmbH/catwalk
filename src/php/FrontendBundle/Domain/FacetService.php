b24ffadd79e35284355834bdf4ff691989abe829
Tobias Schlitt <toby@php.net>
2018-11-28 15:17:04 +0100

    Infrastructure to replicate Facets to Catwalk.

---
+++ src/php/FrontendBundle/Domain/FacetService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Frontastic\Catwalk\FrontendBundle\Domain;
+
+use Frontastic\Common\ReplicatorBundle\Domain\Target;
+
+use Frontastic\Catwalk\FrontendBundle\Gateway\FacetGateway;
+
+class FacetService implements Target
+{
+    /**
+     * @var FacetGateway
+     */
+    private $facetGateway;
+
+    public function __construct(FacetGateway $facetGateway)
+    {
+        $this->facetGateway = $facetGateway;
+    }
+
+    public function lastUpdate(): string
+    {
+        return $this->facetGateway->getHighestSequence();
+    }
+
+    public function replicate(array $updates): void
+    {
+        foreach ($updates as $update) {
+            try {
+                $facet = $this->facetGateway->getEvenIfDeleted($update['facetId']);
+            } catch (\OutOfBoundsException $e) {
+                $facet = new Facet();
+                $facet->facetId = $update['facetId'];
+            }
+
+            $facet = $this->fill($facet, $update);
+            $this->facetGateway->store($facet);
+        }
+    }
+
+    public function fill(Facet $facet, array $data): Facet
+    {
+        $facet->sequence = $data['sequence'];
+        $facet->attributeId = $data['attributeId'];
+        $facet->attributeType = $data['attributeType'];
+        $facet->sort = $data['sort'];
+        $facet->isEnabled = (bool)$data['isEnabled'];
+        $facet->label = (!empty($data['label']) ? (object)$data['label'] : null);
+        $facet->displayType = (!empty($data['displayType']) ? $data['displayType'] : null);
+        $facet->urlIdentifier = (!empty($data['urlIdentifier']) ? $data['urlIdentifier'] : null);
+        $facet->facetOptions = (!empty($data['facetOptions']) ? (object)$data['facetOptions'] : null);
+        $facet->metaData = $data['metaData'];
+        $facet->isDeleted = (bool)$data['isDeleted'];
+
+        return $facet;
+    }
+
+    public function get(string $facetId): Facet
+    {
+        return $this->facetGateway->get($facetId);
+    }
+}
---
+++ src/php/FrontendBundle/Gateway/FacetGateway.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Frontastic\Catwalk\FrontendBundle\Gateway;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
+
+use Frontastic\Catwalk\FrontendBundle\Domain\Facet;
+
+class FacetGateway
+{
+    /**
+     * @var EntityRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManager
+     */
+    private $manager;
+
+    public function __construct(EntityRepository $repository, EntityManager $manager)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
+    public function get(string $facetId): Facet
+    {
+        if (($facet = $this->repository->findOneByFacetId($facetId)) === null) {
+            throw new \OutOfBoundsException("Facet with ID $facetId could not be found.");
+        }
+
+        return $facet;
+    }
+
+    public function getHighestSequence(): string
+    {
+        $query = $this->manager->createQuery(
+            "SELECT
+                MAX(n.sequence)
+            FROM
+                Frontastic\\Catwalk\\FrontendBundle\\Domain\\Facet n"
+        );
+
+        return $query->getSingleScalarResult() ?? '0';
+    }
+
+    public function store(Facet $facet): Facet
+    {
+        $this->manager->persist($facet);
+        $this->manager->flush();
+
+        return $facet;
+    }
+
+    public function remove(Facet $facet)
+    {
+        $facet->isDeleted = true;
+        $this->store($facet);
+    }
+
+    public function getEvenIfDeleted(string $facetId): Facet
+    {
+        return $this->withoutUndeletedFilter(function () use ($facetId) {
+            return $this->get($facetId);
+        });
+    }
+
+    private function withoutUndeletedFilter(callable $callback)
+    {
+        $this->manager->getFilters()->disable('undeleted');
+        try {
+            return $callback();
+        } finally {
+            $this->manager->getFilters()->enable('undeleted');
+        }
+    }
+}
