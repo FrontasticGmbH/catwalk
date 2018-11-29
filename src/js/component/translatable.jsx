448c56f35e8f07e0e87b02b84e11c448b67bfc3f
Tobias Schlitt <toby@php.net>
2018-11-29 15:18:46 +0100

    Added <Translatable /> component to Catwalk.

---
+++ src/js/component/translatable.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getTranslation } from 'frontastic-common'
+
+class Translatable extends Component {
+    render () {
+        let value = getTranslation(
+            this.props.value,
+            this.props.currentLocale,
+            this.props.defaultLocale
+        )
+
+        if (value.locale !== this.props.currentLocale) {
+            // Since the current text is not available in the locale
+            // selected by the user, which is assumed to be the default for
+            // the full document, we explicitely declare the language here
+            // for this node.
+            return <span lang={value.locale} className='untranslated'>{value.text}</span>
+        } else {
+            return value.text
+        }
+    }
+}
+
+Translatable.propTypes = {
+    value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+    ]).isRequired,
+    currentLocale: PropTypes.string.isRequired,
+    defaultLocale: PropTypes.string.isRequired,
+}
+
+Translatable.defaultProps = {
+}
+
+export default connect(
+    (globalState, props) => {
+        return {
+            currentLocale: globalState.app.context.locale,
+            defaultLocale: globalState.app.context.project.defaultLanguage,
+            value: props.value,
+        }
+    }
+)(Translatable)
