a2ad259c43a99c34c814b1783993c2ae01a88415
Tobias Schlitt <toby@php.net>
2018-12-20 17:25:18 +0100

    Implemented UrlState.
    (project merge back 2019-01-08)

--- src/js/app/urlHandler/urlState.js
+++ src/js/app/urlHandler/urlState.js
@@ -2,8 +2,21 @@
 import _ from 'lodash'
 import ProductStreamParameters from './productStreamParameters'
 
+/**
+ * @param {Object} parameters
+ * @param {Object[]} streamConfigurations
+ * @constructor
+ */
 const UrlState = function (parameters, streamConfigurations) {
+    /**
+     * @private
+     * @type {Object}
+     */
     this.parameters = parameters
+    /**
+     * @private
+     * @type {Object{ProductStreamParameters}}
+     */
     this.streamMap = _.mapValues(
         _.keyBy(streamConfigurations, 'streamId'),
         (streamConfiguration) => {
@@ -15,12 +28,53 @@ const UrlState = function (parameters, streamConfigurations) {
         }
     )
 
+    /**
+     * @param {string} streamId
+     * @return {ProductStreamParameters}
+     */
     this.getStream = (streamId) => {
         if (!this.streamMap[streamId]) {
             throw new Error('Unknown stream with ID ' + streamId)
         }
 
         return this.streamMap[streamId]
+    }
+
+    /**
+     * @return {Object}
+     */
+    this.getParameters = () => {
+        let parameters = _.cloneDeep(this.parameters)
+
+        parameters.s = _.mapValues(
+            this.streamMap,
+            /**
+             *
+             * @param {ProductStreamParameters} stream
+             * @return {Object}
+             */
+            (stream) => {
+                return stream.getParameters()
+            }
+        )
+
+        const crawlable = _.reduce(
+            _.map(this.streamMap, (stream) => {
+                return stream.isCrawlable()
+            }),
+            (aggregateCrawlable, isCrawlable) => {
+                return aggregateCrawlable && isCrawlable
+            },
+            true
+        )
+
+        if (crawlable) {
+            delete parameters.nocrawl
+        } else {
+            parameters.nocrawl = '1'
+        }
+
+        return parameters
     }
 }
 
--- test/js/app/urlHandler/urlStateTest.js
+++ test/js/app/urlHandler/urlStateTest.js
@@ -5,9 +5,7 @@ test('it creates stream parameters', () => {
     const urlState = new UrlState(
         {},
         [
-            {
-                streamId: 'foo',
-            }
+            { streamId: 'foo', }
         ]
     )
 
@@ -18,13 +16,77 @@ test('it errors on missing stream', () => {
     const urlState = new UrlState(
         {},
         [
-            {
-                streamId: 'foo',
-            }
+            { streamId: 'foo', }
         ]
     )
 
     expect(() => {
         urlState.getStream('bar')
     }).toThrow()
+})
+
+test('it collects parameters', () => {
+    const urlState = new UrlState(
+        {
+            someOther: 23,
+            s: {
+                foo: {
+                    offset: 42,
+                },
+            },
+        },
+        [
+            { streamId: 'foo', },
+            { streamId: 'bar', },
+        ]
+    )
+
+    expect(urlState.getParameters()).toEqual({
+        someOther: 23,
+        s: {
+            foo: {
+                offset: 42,
+            },
+            bar: {}
+        },
+    })
+})
+
+test('it sets nocrawl parameter', () => {
+    const urlState = new UrlState(
+        {
+            s: {
+                foo: {
+                    facets: { color: 'red' }
+                },
+            },
+        },
+        [
+            { streamId: 'foo', },
+        ]
+    )
+
+    expect(urlState.getParameters()).toMatchObject({
+        nocrawl: '1',
+    })
+})
+
+test('it remove nocrawl parameter', () => {
+    const urlState = new UrlState(
+        {
+            nocrawl: '1',
+            s: {
+                foo: {
+                    offset: 23
+                },
+            },
+        },
+        [
+            { streamId: 'foo', },
+        ]
+    )
+
+    expect(urlState.getParameters()).not.toMatchObject({
+        nocrawl: '1',
+    })
 })
