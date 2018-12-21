e2b00cea42c48116faa3548a130d7bf1ecda68d8
Tobias Schlitt <toby@php.net>
2018-12-21 08:58:49 +0100

    Implemented UrlHandler.
    (project merge back 2019-01-08)

---
+++ src/js/app/urlHandler.js
@@ -0,0 +1,35 @@
+import _ from 'lodash'
+
+import Route from './route'
+import UrlState from './urlHandler/urlState'
+/**
+ * @param {Route} route
+ * @param {object} streamConfigurations
+ * @constructor
+ */
+let UrlHandler = function (route, streamConfigurations) {
+    this.route = route
+    this.streamConfigurations = streamConfigurations
+
+    /**
+     * @param {manipulatorCallback} manipulator
+     */
+    this.getParameters = (manipulator) => {
+        const urlState = new UrlState(
+            _.cloneDeep(this.route.getParameters()),
+            this.streamConfigurations
+        )
+
+        manipulator(urlState)
+
+        return urlState.getParameters()
+    }
+
+}
+
+export default UrlHandler
+
+/**
+ * @callback manipulatorCallback
+ * @param {UrlState} urlState
+ */
---
+++ test/js/app/urlHandlerTest.js
@@ -0,0 +1,45 @@
+import _ from 'lodash'
+
+import UrlHandler from '../../../src/js/app/urlHandler'
+import UrlState from '../../../src/js/app/urlHandler/urlState'
+
+it('it creates new parameters', () => {
+
+    const inputParameters = {
+        some: 'value'
+    }
+    const originalInputParemeters = _.cloneDeep(inputParameters)
+
+    const routeMock = jest.genMockFromModule('../../../src/js/app/route').default
+    routeMock.getParameters = jest.fn(() => {
+        return inputParameters
+    })
+
+    const handler = new UrlHandler(
+        routeMock,
+        [
+            { streamId: 'someStream' },
+        ]
+    )
+
+    const outputParameters = handler.getParameters((urlState) => {
+        expect(urlState).toBeInstanceOf(UrlState)
+
+        urlState.getStream('someStream').setOffset(23)
+    })
+
+    expect(outputParameters).toMatchObject({
+        some: 'value',
+        s: {
+            someStream: {
+                offset: 23,
+            },
+        },
+    })
+
+    // Should not be the same object
+    expect(outputParameters).not.toBe(inputParameters)
+
+    // No change to inputParameters
+    expect(inputParameters).toEqual(originalInputParemeters)
+})
