7d6ca53349865b7ca94847ea0c95fbb4251791e0
Hans-Christian Otto <c.otto@suora.com>
2019-01-10 12:55:23 +0100

    Added Suggestion for NodeLink Component
    (project merge back 2019-05-06)

---
+++ src/js/app/nodeLink.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+import SymfonyLink from "./link"
+
+/**
+ * Supports linking to a frontastic node
+ *
+ * @example
+ * <NodeLink node={imprintNode}>Imprint</NodeLink>
+ */
+const NodeLink = ({node, children, ...otherProps}) => (
+    <SymfonyLink {...otherProps} route={'node_' + node.nodeId}>
+        {children}
+    </SymfonyLink>
+)
+
+NodeLink.propTypes = {
+    children: PropTypes.node,
+    params: PropTypes.object,
+}
+
+export default NodeLink
