7c4dfb7eba0ad7c21c123329c5f2bdbaa38b7dbe
Kore Nordmann <mail@kore-nordmann.de>
2019-01-11 09:11:37 +0100

    Added missing use statement
    (project merge back 2019-05-06)

--- src/php/FrontendBundle/Controller/PatternLibraryController.php
+++ src/php/FrontendBundle/Controller/PatternLibraryController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
+use Frontastic\Catwalk\ApiCoreBundle\Domain\Context;
+
 class PatternLibraryController extends Controller
 {
     public function indexAction(Context $context)
