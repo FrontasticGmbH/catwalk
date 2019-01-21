d1fbe171c13ad455282d8fa9605dbd8f6621f666
Hans-Christian Otto <c.otto@suora.com>
2019-01-21 15:39:19 +0100

    Added „Termin Vereinbaren“ to mobile menu
    (project merge back 2019-05-06)

--- src/js/app/referenceHelper.js
+++ src/js/app/referenceHelper.js
@@ -9,3 +9,33 @@ export const isReferenceAbsoluteHttpLink = (reference) => {
     const target = reference.target
     return target.startsWith('http://') || target.startsWith('https://')
 }
+
+/**
+ * @returns string
+ */
+export const getPathForAppAndReference = (router, reference) => {
+    if (isReferenceAbsoluteHttpLink(reference)) {
+        throw new Error(`Reference to ${reference.target} is an absolute link, which is not supported by getPathForAppAndReference`);
+    }
+    if (reference.type === 'link') {
+        return reference.target;
+    }
+
+    if (router.hasRoute(`node_${reference.target}`)) {
+        return router.path(`node_${reference.target}`)
+    }
+
+    return '/__error'
+}
+
+/**
+ * Navigate the user to the site corresponding to the given reference
+ */
+export const gotoReference = (router, reference) => {
+    if (isReferenceAbsoluteHttpLink(reference)) {
+        window.location.href = reference.target;
+        return;
+    }
+
+    router.history.push(getPathForAppAndReference(router, reference));
+}
