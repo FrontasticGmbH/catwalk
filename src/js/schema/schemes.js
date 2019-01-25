9b1e59bae91a87f63b4e9433a1e1e4c8f2cbeca6
Tobias Schlitt <toby@php.net>
2019-01-25 14:03:36 +0100

    Moved default schemas to libraries/common for use in Backstage & Catwalk.

--- src/js/schema/schemes.js
+++ src/js/schema/schemes.js
@@ -1,9 +1,12 @@
-import NodeConfiguration from './nodeConfiguration'
+import { DefaultSchemas } from 'frontastic-common'
 
 import ComponentInjector from '../app/injector'
 
 const schemes = {
-    node: ComponentInjector.return('NodeConfigurationSchema', NodeConfiguration),
+    node: ComponentInjector.return(
+        'NodeConfigurationSchema',
+        DefaultSchemas.NodeConfigurationSchema
+    ),
 }
 
 export default schemes
