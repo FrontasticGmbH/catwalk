daf4923f7ac58d712d13e28ea57acdbd89772bb0
Marcel Fahle <m.fahle@gmail.com>
2019-03-08 13:51:33 +0100

    Added LinkTileHero component
    (project merge back 2019-05-06)

--- src/js/component/reference.jsx
+++ src/js/component/reference.jsx
@@ -2,40 +2,47 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import Link from '../app/link'
-import {isReferenceAbsoluteHttpLink} from "../app/referenceHelper";
+import { isReferenceAbsoluteHttpLink } from '../app/referenceHelper'
 
 /**
  * Provides a Link to a Reference (which means either a link to an external site or a node).
  */
 class Reference extends Component {
-    render () {
-        if (!this.props.reference.type) {
-            return <Fragment>{this.props.children}</Fragment>
+    render() {
+        const { reference, children, className, style, target } = this.props
+        if (!reference.type) {
+            return <Fragment>{children}</Fragment>
         }
 
-        if (isReferenceAbsoluteHttpLink(this.props.reference)) {
-            return <a href={this.props.reference.target} className={this.props.className}>{this.props.children}</a>
+        if (isReferenceAbsoluteHttpLink(reference)) {
+            return (
+                <a href={reference.target} className={className} style={style} target={target}>
+                    {children}
+                </a>
+            )
         }
 
         return (
-            <Link children={this.props.children} {...this.linkProps()} />
+            <Link className={className} {...this.linkProps()} style={style} target={target}>
+                {children}
+            </Link>
         )
     }
 
     linkProps = () => {
         switch (this.props.reference.type) {
-        case 'link':
-            return {
-                path: this.props.reference.target,
-            }
+            case 'link':
+                return {
+                    path: this.props.reference.target,
+                }
 
-        case 'node':
-            return {
-                route: 'node_' + this.props.reference.target,
-            }
+            case 'node':
+                return {
+                    route: 'node_' + this.props.reference.target,
+                }
 
-        default:
-            return {}
+            default:
+                return {}
         }
     }
 }
@@ -43,6 +50,9 @@ class Reference extends Component {
 Reference.propTypes = {
     reference: PropTypes.object.isRequired,
     children: PropTypes.any,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    target: PropTypes.string,
 }
 
 Reference.defaultProps = {}
