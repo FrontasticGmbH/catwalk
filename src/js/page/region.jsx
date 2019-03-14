b76787c7f2318ae84ec20da130e023a61a2a001a
Marcel Fahle <m.fahle@gmail.com>
2019-03-14 16:43:54 +0100

    Added rows to page render (work in progress)
    (project merge back 2019-05-06)

--- src/js/page/region.jsx
+++ src/js/page/region.jsx
@@ -5,6 +5,7 @@ import { ConfigurationSchema } from 'frontastic-common'
 import configurationResolver from '../app/configurationResolver'
 
 import Grid from '../patterns/atoms/grid/grid'
+import Row from '../patterns/atoms/grid/row'
 import Cell from './cell'
 import schemas from '../schemas'
 
@@ -15,6 +16,31 @@ class Region extends React.Component {
         let region = this.props.region
         if (!region) {
             return null
+        }
+
+        // TBR (to be refactored) *marcel
+        // very readable like this (prio) but might be
+        // better suited in a function
+        let colsPerRow = 0,
+            row = 0,
+            renderTree = [[]]
+        // elements?
+        if (region && region.elements && region.elements.length) {
+            const els = region.elements
+            // iterate
+            for (let i = 0; i < els.length; i++) {
+                // is nested array?
+                if (!renderTree[row]) renderTree[row] = []
+                // push cell to nested array
+                renderTree[row].push(els[i])
+                // add current col size to counter
+                colsPerRow += els[i].schema.get('size') || '12'
+                // row full (or exceeded)? add new row, reset counter.
+                if (colsPerRow >= 12) {
+                    row++
+                    colsPerRow = 0
+                }
+            }
         }
 
         return (
@@ -30,27 +56,32 @@ class Region extends React.Component {
                     outline: region.regionId === this.props.highlight ? '2px dashed #a1d8e7' : null,
                 }}
             >
-                {region && region.elements && region.elements.length
-                    ? region.elements.map((cell, i) => {
-                          const customConfiguration = configurationResolver(
-                              new ConfigurationSchema(schemas.cell.schema, cell.customConfiguration || {}),
-                              (this.props.data.data || {}).stream || {}
-                          )
+                {renderTree.map((group, i) => {
+                    // TODO: fix row key
+                    return (
+                        <Row key={`row_${i}`}>
+                            {group.map((cell) => {
+                                const customConfiguration = configurationResolver(
+                                    new ConfigurationSchema(schemas.cell.schema, cell.customConfiguration || {}),
+                                    (this.props.data.data || {}).stream || {}
+                                )
 
-                          return (
-                              <Cell
-                                  key={cell.cellId}
-                                  highlight={this.props.highlight}
-                                  node={this.props.node}
-                                  page={this.props.page}
-                                  data={this.props.data}
-                                  region={region}
-                                  cell={cell}
-                                  customConfiguration={customConfiguration}
-                              />
-                          )
-                      })
-                    : null}
+                                return (
+                                    <Cell
+                                        key={cell.cellId}
+                                        highlight={this.props.highlight}
+                                        node={this.props.node}
+                                        page={this.props.page}
+                                        data={this.props.data}
+                                        region={region}
+                                        cell={cell}
+                                        customConfiguration={customConfiguration}
+                                    />
+                                )
+                            })}
+                        </Row>
+                    )
+                })}
             </Grid>
         )
     }
---
+++ src/js/patterns/atoms/grid/row.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Grid from './grid'
+import Cell from './cell'
+
+export default function Row({ children }) {
+    return (
+        <Cell size={12}>
+            <Grid>{children}</Grid>
+        </Cell>
+    )
+}
