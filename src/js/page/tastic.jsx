c952664f7d33c6223abbcc2143b393d89a69e344
Kore Nordmann <mail@kore-nordmann.de>
2019-03-19 13:12:45 +0100

    Check for window object properly
    (project merge back 2019-05-06)

--- src/js/page/tastic.jsx
+++ src/js/page/tastic.jsx
@@ -7,7 +7,7 @@ import ErrorBoundary from '../app/errorBoundary'
 import tasticDataConnector from './tasticDataConnector'
 
 class Tastic extends Component {
-    tastics = (typeof window !== 'undefined') ? window.tastics : global.tastics
+    tastics = window ? window.tastics : global.tastics
 
     render () {
         let tastic = this.props.tastic
