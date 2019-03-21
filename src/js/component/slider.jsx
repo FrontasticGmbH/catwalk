23b331a3303d70a7f65a916fb847841a3f449233
Kore Nordmann <mail@kore-nordmann.de>
2019-03-21 07:24:28 +0100

    Import Flickity dynamically to get SSR working
    Also use the same refactored Slider component in catwalk, because it now
    works far more reliable.
    
    (project merge back 2019-05-06)

--- src/js/component/slider.jsx
+++ src/js/component/slider.jsx
@@ -1,95 +1,108 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 import PropTypes from 'prop-types'
-import Flickity from 'flickity'
-import imagesLoaded from 'imagesloaded'
-import _ from 'lodash'
+// import Flickity from 'react-flickity-component' // Imported dynamically in constructor
 
-import ComponentInjector from '../app/injector'
+const defaultOptions = {
+    cellAlign: 'left',
+    pageDots: true,
+    prevNextButtons: true,
+    groupCells: true,
+    draggable: '>1',
+    arrowShape:
+        'M44.2,45.1l31.9-31.9c3-3,3-7.7,0-10.6l-0.4-0.4c-3-3-7.7-3-10.6,0L22.6,44.7 c-3,3-3,7.7,0,10.6l42.5,42.5c3,3,7.7,3,10.6,0l0,0c3-3,3-7.7,0-10.6L44.2,55.7C41.3,52.8,41.3,48,44.2,45.1z',
+}
 
 class Slider extends Component {
     constructor (props) {
         super(props)
 
-        this.state = {
-            selected: 0,
-            error: null,
+        if ((typeof window !== 'undefined') && window) {
+            this.Flickity = require('react-flickity-component')
+        }
+    }
+
+    Flickity = null
+
+    updateDimensions = () => {
+        const elements = this.carousel.slider.children
+        this.carousel.slider.classList.add('slider-is-resizing')
+
+        clearTimeout(resizeTimer)
+
+        this.carousel.resize()
+        const resizeTimer = setTimeout(() => {
+            this.carousel.slider.classList.remove('slider-is-resizing')
+            this.carousel.resize()
+            this.carousel.reloadCells()
+        }, 300)
+    }
+
+
+    checkSliderAlone = () => {
+        if (this.carousel && this.carousel.slides) {
+            this.carousel.slides.length < 2
+                ? this.carousel.element.classList.add('slider-is-alone')
+                : this.carousel.element.classList.remove('slider-is-alone')
         }
     }
 
     componentDidMount = () => {
-        this.flickity = new Flickity(this.refs.carousel, _.extend(
-            {
-                cellSelector: '.c-slider__item',
-                cellAlign: 'left',
-                pageDots: false,
-                prevNextButtons: false,
-                draggable: '>1',
-            },
-            this.props.options
-        ))
-
-        this.flickity.on('cellSelect', this.updateSelected)
-
-        if (this.props.imagesLoaded) {
-            imagesLoaded(this.refs.carousel, () => {
-                this.flickity.resize()
-            })
-        }
-    }
-
-    componentDidUpdate = (nextProps) => {
-        if (_.isEqual(nextProps.children, this.props.children)) {
-            return
-        }
-
-        this.flickity.reloadCells()
-        if (this.props.imagesLoaded) {
-            imagesLoaded(this.refs.carousel, () => {
-                this.flickity.resize()
-            })
-        }
-    }
-
-    componentWillUnmount = () => {
-        if (this.flickity) {
-            this.flickity.off('cellSelect', this.updateSelected)
-            this.flickity.destroy()
-        }
-    }
-
-    updateSelected = () => {
-        this.setState({
-            selected: this.flickity.selectedIndex,
-        })
-    }
-
-    render () {
         // @TODO: How to render Flickity on SSR?
-        if (!window || !Flickity) {
+        if (!window) {
             return null
         }
 
-        return (<div className={this.props.className} ref='carousel'>
-            <div className='flickity-viewport'>
-                <div className='flickity-slider'>
-                    {this.props.children}
-                </div>
-            </div>
-        </div>)
+        window.addEventListener('resize', () => {
+            this.updateDimensions()
+            this.checkSliderAlone()
+        })
+        this.forceUpdate()
+
+        this.carousel.on('ready', this.checkSliderAlone);
+    }
+
+    componentWillUnmount() {
+        // @TODO: How to render Flickity on SSR?
+        if (!window) {
+            return null
+        }
+
+        window.removeEventListener('resize', this.updateDimensions)
+    }
+
+    render() {
+        // @TODO: How to render Flickity on SSR?
+        if (!window || !this.Flickity) {
+            return null
+        }
+
+        const { options, className, children, slidesPerPage } = this.props
+        const Flickity = this.Flickity
+
+        return (
+            <Flickity
+                className={classnames('c-slider', slidesPerPage ? `c-slider--${slidesPerPage}` : '', className)}
+                options={{ ...defaultOptions, ...options }}
+                reloadOnUpdate
+                flickityRef={(carousel) => (this.carousel = carousel)}
+            >
+                {children}
+            </Flickity>
+        )
     }
 }
 
 Slider.propTypes = {
-    imagesLoaded: PropTypes.bool,
     options: PropTypes.object,
     className: PropTypes.string,
+    slidesPerPage: PropTypes.oneOf([1, 2, 3, 4]),
     children: PropTypes.array,
 }
 
 Slider.defaultProps = {
-    imagesLoaded: true,
-    options: {},
-    className: 'c-slider js-slider',
+    slidesPerPage: 1,
+    defaultOptions,
 }
 
 export default ComponentInjector.return('Slider', Slider)
