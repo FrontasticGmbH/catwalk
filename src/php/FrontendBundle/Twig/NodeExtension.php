1d2b24e4f6c0b487a3f42635917cce51a61fd18e
Manuel Pichler <github@manuel-pichler.de>
2019-03-26 08:54:00 +0100

    Remove hard dep on services in twig extension
    (project merge back 2019-05-06)

--- src/php/FrontendBundle/Twig/NodeExtension.php
+++ src/php/FrontendBundle/Twig/NodeExtension.php
@@ -8,37 +8,30 @@ use Frontastic\Catwalk\FrontendBundle\Domain\FacetService;
 use Frontastic\Common\ProductApiBundle\Domain\ProductApi;
 use Frontastic\Common\ProductApiBundle\Domain\ProductApi\Query\CategoryQuery;
 use Frontastic\Catwalk\ApiCoreBundle\Domain\Context;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class NodeExtension extends \Twig_Extension
 {
-    private $nodeService;
-
-    private $tasticService;
-
     private $facetService;
 
     private $productApi;
 
     private $context;
 
-    public function __construct(
-        NodeService $nodeService,
-        TasticService $tasticService,
-        FacetService $facetService,
-        ProductApi $productApi,
-        Context $context
-    ) {
-        $this->nodeService = $nodeService;
-        $this->tasticService = $tasticService;
-        $this->facetService = $facetService;
-        $this->productApi = $productApi;
-        $this->context = $context;
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
     }
 
     public function getFunctions(): array
     {
         return [
-            new \Twig_Function('frontastic_tree', [$this->nodeService, 'getTree']),
+            new \Twig_Function('frontastic_tree', [$this->container->get(NodeService::class), 'getTree']),
             new \Twig_Function('completeInformation', [$this, 'completeInformation']),
         ];
     }
@@ -50,12 +43,14 @@ class NodeExtension extends \Twig_Extension
                 'node' => null,
                 'page' => null,
                 'data' => null,
-                'tastics' => $this->tasticService->getAll(),
+                'tastics' => $this->container->get(TasticService::class)->getAll(),
                 'facets' => $this->facetService->getEnabled(),
-                'categories' => $this->productApi->getCategories(new CategoryQuery([
-                    'locale' => $this->context->locale,
-                    'limit' => 100,
-                ])),
+                'categories' => $this->container->get(ProductApi::class)->getCategories(
+                    new CategoryQuery([
+                        'locale' => $this->container->get(Context::class)->locale,
+                        'limit' => 100,
+                    ])
+                ),
             ],
             $variables
         );
