334db1e2fd88d301b397a7a749f901b9473cb60d
Tobias Schlitt <toby@php.net>
2019-03-29 09:00:04 +0100

    Extracted title generation into re-usable function.
    (project merge back 2019-05-06)

---
+++ src/js/node/generateTitle.js
@@ -0,0 +1,19 @@
+import { getTranslation } from 'frontastic-common'
+
+function generateTitle (node, context) {
+    if (node.configuration.seoTitle) {
+        return getTranslation(
+            node.configuration.seoTitle,
+            context.locale,
+            context.project.defaultLanguage
+        ).text
+    }
+
+    if (node.name) {
+        return node.name
+    }
+
+    return node.configuration.path || ''
+}
+
+export default generateTitle
