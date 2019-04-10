92d1b09fa5d990efa7aafeb9525c0dc00922e6df
Marcel Fahle <m.fahle@gmail.com>
2019-04-10 07:13:19 +0200

    Updates Grid view of product listing page
    (project merge back 2019-05-06)

--- src/js/tastic/productList/tastic.jsx
+++ src/js/tastic/productList/tastic.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 
+import Grid from '../../patterns/atoms/grid/grid'
+import Cell from '../../patterns/atoms/grid/cell'
 import MoleculesProductTeaser from '../../patterns/molecules/teasers/product-teaser'
 
 class ProductListTastic extends Component {
-    render () {
+    render() {
         let productList = this.props.rawData.stream[this.props.tastic.configuration.stream]
         if (!productList) {
             return null
@@ -14,17 +16,23 @@ class ProductListTastic extends Component {
         let showPercent = this.props.tastic.schema.get('showPercent') || true
         let showStrikePrice = this.props.tastic.schema.get('showStrikePrice') || true
 
-        return (<div className='o-layout'>
-            {_.map(productList.items, (product) => {
-                return (<div key={product.productId} className='o-layout__item u-1/2 u-1/3@lap u-1/4@desk'>
-                    <MoleculesProductTeaser
-                        product={product}
-                        showPercent={showPercent}
-                        showStrikePrice={showStrikePrice}
-                    />
-                </div>)
-            })}
-        </div>)
+        /* TODO: move to proper pattern/tastic in apollo_de *marcel */
+
+        return (
+            <Grid className='c-product-listing'>
+                {_.map(productList.items, (product) => {
+                    return (
+                        <div key={product.productId} className='c-product-listing__item'>
+                            <MoleculesProductTeaser
+                                product={product}
+                                showPercent={showPercent}
+                                showStrikePrice={showStrikePrice}
+                            />
+                        </div>
+                    )
+                })}
+            </Grid>
+        )
     }
 }
 
@@ -33,7 +41,6 @@ ProductListTastic.propTypes = {
     tastic: PropTypes.object.isRequired,
 }
 
-ProductListTastic.defaultProps = {
-}
+ProductListTastic.defaultProps = {}
 
 export default ProductListTastic
