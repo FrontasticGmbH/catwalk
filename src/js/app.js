74ec69f3df486ca3e25d08d559686941aeb74c30
Kore Nordmann <mail@kore-nordmann.de>
2019-04-11 11:12:04 +0200

    Remove flickering by removing routes
    (project merge back 2019-05-06)

--- src/js/app.js
+++ src/js/app.js
@@ -63,45 +63,6 @@ export default (mountNode, tastics = null) => {
                             component={Patterns}
                         />
 
-                        {_.toArray(
-                            _.mapValues(app.getRouter().routes, (path, route) => {
-                                if (route.substr(0, 5) !== 'node_') {
-                                    return null
-                                }
-
-                                return (
-                                    <Route
-                                        exact
-                                        key={route}
-                                        path={app.getRouter().reactRoute(route)}
-                                        component={Node}
-                                    />
-                                )
-                            })
-                        )}
-
-                        <Route
-                            exact
-                            path='/'
-                            component={() => {
-                                return (
-                                    <div
-                                        style={{
-                                            maxWidth: '768px',
-                                            margin: '50px auto',
-                                        }}
-                                    >
-                                        <h1 className='c-heading-beta'>Frontastic Local Development</h1>
-                                        <div className='c-alert c-alert--info'>
-                                            <p className='c-alert__message'>
-                                                You can find <a href='/_patterns'>our Patternlab under /_patterns</a>.
-                                            </p>
-                                        </div>
-                                    </div>
-                                )
-                            }}
-                        />
-
                         <Route component={Node} />
                     </Switch>
                 </Router>
