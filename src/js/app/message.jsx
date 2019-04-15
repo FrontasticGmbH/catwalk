da1a6cbe65eb80588eec7de1dad08530e25c44d2
Hans-Christian Otto <c.otto@suora.com>
2019-04-15 19:29:49 +0200

    Resillience in Message Component
    This fixes an issue during registration
    
    (project merge back 2019-05-06)

--- src/js/app/message.jsx
+++ src/js/app/message.jsx
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl'
 
 class Message extends Component {
     render() {
+        if (!this.props.code) {
+            console.warn(`FormattedMessage is missing an id, and will thus not be translated: ${this.props.message}`)
+            return this.props.message
+        }
         return (
-            <FormattedMessage
-                id={this.props.code}
-                defaultMessage={this.props.message}
-                values={this.props.parameters}
-            />
-        );
+            <FormattedMessage id={this.props.code} defaultMessage={this.props.message} values={this.props.parameters} />
+        )
     }
 }
 
 Message.propTypes = {
     message: PropTypes.string.isRequired,
-    code: PropTypes.string,
+    code: PropTypes.string.isRequired,
     parameters: PropTypes.object,
 }
 
