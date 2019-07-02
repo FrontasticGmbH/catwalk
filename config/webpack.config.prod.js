95dcfc31b0762f78bd252993118e6f95c0a65e90
Hans-Christian Otto <c.otto@suora.com>
2019-07-02 08:00:57 +0200

    Prefer .jsx over .json in webpack :-)

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -94,7 +94,7 @@ const mainConfig = {
         // https://github.com/facebookincubator/create-react-app/issues/290
         // `web` extension prefixes have been added for better support
         // for React Native Web.
-        extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
+        extensions: ['.web.js', '.js', '.jsx', '.json', '.web.jsx'],
         alias: {
             // Support React Native Web
             // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
