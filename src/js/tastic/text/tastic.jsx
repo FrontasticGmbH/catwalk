b58f670ab667ad99c5df126285c825c703cea53a
Hans-Christian Otto <c.otto@suora.com>
2019-07-05 19:11:03 +0200

    Use `withTranslatedTasticData` in `TextTastic`
    I do not have a VM running right now, so this commit was not yet verified to work.

--- src/js/tastic/text/tastic.jsx
+++ src/js/tastic/text/tastic.jsx
@@ -1,33 +1,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { getTranslation } from 'frontastic-common'
+import {withTranslatedTasticData} from "../../component/withTranslatedTasticData";
 
 class TextTastic extends Component {
     render () {
-        let text = getTranslation(
-            this.props.tastic.schema.get('text'),
-            this.props.context.locale,
-            this.props.context.project.defaultLanguage
-        )
-
-        return <div className='text'>{text.text}</div>
+        const {text} = this.props
+        return <div className='text'>{text}</div>
     }
 }
 
 TextTastic.propTypes = {
-    context: PropTypes.object.isRequired,
     tastic: PropTypes.object.isRequired,
 }
 
-TextTastic.defaultProps = {
-}
-
-export default connect(
-    (globalState, props) => {
-        return {
-            context: globalState.app.context,
-            ...props,
-        }
-    }
-)(TextTastic)
+export default withTranslatedTasticData(TextTastic)
