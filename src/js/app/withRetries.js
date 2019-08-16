8f67a2033cfd41323bc327052c249f4667df6c53
Tobias Schlitt <toby@php.net>
2019-08-16 09:43:05 +0200

    Clarified parameter name.

--- src/js/app/withRetries.js
+++ src/js/app/withRetries.js
@@ -1,11 +1,11 @@
 /**
  *
  * @param {function} callback to execute, receives retry() function to schedule a retry
- * @param {int} trialCount how often a retry should be tried
+ * @param {int} tryCount how often a retry should be tried
  * @param {function} callback to be executed if trials ran out without success
  */
-const withRetries = (callback, trialCount, giveUpCallback = null) => {
-    if (trialCount === 0) {
+const withRetries = (callback, tryCount, giveUpCallback = null) => {
+    if (tryCount === 0) {
         if (giveUpCallback !== null) {
             giveUpCallback()
         }
@@ -13,7 +13,7 @@ const withRetries = (callback, trialCount, giveUpCallback = null) => {
     }
 
     const retry = () => {
-        withRetries(callback, trialCount - 1, giveUpCallback)
+        withRetries(callback, tryCount - 1, giveUpCallback)
     }
 
     callback(retry)
