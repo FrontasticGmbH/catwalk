f1050efaaf76e09a11248feab4b1c518ac5773b4
Hans-Christian Otto <c.otto@suora.com>
2019-09-09 10:30:08 +0200

    Not using tasticDataConnector anymore
    We now have a memoized version within the TasticWrapper-Component itself.
    This way we could use the useMemo-hook, which was more sensible then some memoization-library.

--- src/js/page/tastic.jsx
+++ src/js/page/tastic.jsx
@@ -1,16 +1,48 @@
-import React, { Component } from 'react'
+import React, { useMemo } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 
 import ErrorBoundary from '../app/errorBoundary'
 import tasticDataConnector from './tasticDataConnector'
+import configurationResolver from '../app/configurationResolver';
+
+const getStreamIdsForTasticSchema = (schema) => {
+    return Object.keys(schema.fields)
+        .filter(fieldName => {
+            return schema.fields[fieldName].type === 'stream'
+        })
+        .map(fieldName => schema.get(fieldName))
+}
 
 const TasticWrapper = (props) => {
     const tastics = (window && window.tastics) || (global && global.tastics) || []
     const tastic = props.tastic
 
-    if (!tastics[tastic.tasticType]) {
+    if (!props.tastic || !props.data) {
+        // eslint-disable-next-line no-console
+        console.error('Could not render tastic, because tastic data was missing')
+        return null;
+    }
+
+    const additionalData = (props.data.tastic || {})[props.tastic.tasticId] || null
+
+    const streams = getStreamIdsForTasticSchema(props.tastic.schema)
+        .map(streamId => props.data.stream[streamId])
+
+    const tasticData = useMemo(() => {
+        return configurationResolver(
+            props.tastic.schema,
+            props.data.stream,
+            additionalData || {}
+        )
+
+        // Warning - the dependencies of this useMemo might differ from those that are actually in use,
+        // because it makes assumptions about the implementation details of configurationResolver.
+        // @TODO This should be improved in the future.
+    }, props.tastic.schema, props.data.stream, additionalData, ...streams)
+
+  if (!tastics[tastic.tasticType]) {
         if (!props.isDebug) {
             return null
         }
@@ -33,7 +65,7 @@ const TasticWrapper = (props) => {
                 outline: (_.isEqual(tastic.tasticId, props.highlight) ? '2px dashed #d73964' : null),
             }}
             id={tastic.tasticId}>
-            <Tastic tastic={tastic} node={props.node} page={props.page} rawData={props.data} data={props.resolved} />
+            <Tastic tastic={tastic} node={props.node} page={props.page} rawData={props.data} data={tasticData} />
         </div>
     </ErrorBoundary>)
 }
@@ -43,7 +75,6 @@ TasticWrapper.propTypes = {
     page: PropTypes.object.isRequired,
     tastic: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
-    resolved: PropTypes.object.isRequired,
     highlight: PropTypes.any,
     isDebug: PropTypes.bool,
 }
@@ -52,4 +83,8 @@ TasticWrapper.defaultProps = {
     isDebug: false,
 }
 
-export default connect(tasticDataConnector)(TasticWrapper)
+export default connect((globalState) => {
+    return  {
+        isDebug: !!(globalState.app.context && globalState.app.context.isDevelopment())
+    }
+})(TasticWrapper)
