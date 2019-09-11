fc2600ab5d9d7ad57c000a80f0d7749f241f8984
Hans-Christian Otto <c.otto@suora.com>
2019-09-11 12:46:37 +0200

    Remove unnecessarsy dom nodes from `<Translatable />`

--- src/js/component/translatable.jsx
+++ src/js/component/translatable.jsx
@@ -15,11 +15,15 @@ class Translatable extends Component {
             this.props.defaultLocale
         )
 
+        // For now, we are disabling the rendering of the <span /> to reduce dom size.
+        // In the future, this will depend on the debug flag.
+        return value.text
         if (value.locale !== this.props.currentLocale) {
             // Since the current text is not available in the locale
             // selected by the user, which is assumed to be the default for
             // the full document, we explicitely declare the language here
             // for this node.
+            // return <span lang={value.locale} className='untranslated'>{value.text}</span>
             return <span lang={value.locale} className='untranslated'>{value.text}</span>
         } else {
             return value.text
