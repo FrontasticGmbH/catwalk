d26a5d2d814524de838cf6d684e8918929682811
Manuel Pichler <github@manuel-pichler.de>
2019-09-18 17:01:22 +0200

    Only overwrite http status code when not already set.

--- src/php/FrontendBundle/Domain/RenderService/ResponseDecorator.php
+++ src/php/FrontendBundle/Domain/RenderService/ResponseDecorator.php
@@ -2,27 +2,50 @@
 
 namespace Frontastic\Catwalk\FrontendBundle\Domain\RenderService;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ResponseDecorator
 {
+    const NO_STATUS_CODE = -23;
+
     /**
-     * @var bool
+     * @var int
      */
-    private $timedOut = false;
+    private $statusCode = self::NO_STATUS_CODE;
 
     public function setTimedOut()
     {
-        $this->timedOut = true;
+        if (self::NO_STATUS_CODE === $this->statusCode) {
+            $this->statusCode = Response::HTTP_SERVICE_UNAVAILABLE;
+        }
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $this->statusCode = $exception->getStatusCode();
+        } else {
+            $this->statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
     }
 
     public function onKernelResponse(FilterResponseEvent $event)
     {
-        if (!$this->timedOut) {
+        // No status changes required
+        if (self::NO_STATUS_CODE === $this->statusCode) {
             return;
         }
-
         $response = $event->getResponse();
-        $response->setStatusCode(503);
+
+        // There is already an error status set
+        if ($response->getStatusCode() >= 400) {
+            return;
+        }
+        $response->setStatusCode($this->statusCode);
     }
 }
