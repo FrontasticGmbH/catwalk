555035aa1a73dcfbe74daacf4996969e70b154cb
Kore Nordmann <mail@kore-nordmann.de>
2019-11-15 07:26:06 +0100

    Also add svgr loader to server builds

--- config/webpack.server.production.js
+++ config/webpack.server.production.js
@@ -8,6 +8,7 @@ let config = require('./webpack.js')(PRODUCTION, SERVER)
 config = require('./webpack/ignoreScss.js')(config)
 config = require('./webpack/provideDomOnServer.js')(config)
 config = require('./webpack/singleChunk.js')(config)
+config = require('./webpack/svgr.js')(config)
 
 config.optimization = { minimize: true }
 config.output.filename = 'assets/js/server.js'
