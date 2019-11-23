21b933349b84894bf3c4f3cfb1e069eb494003e3
Marcel Fahle <m.fahle@gmail.com>
2019-11-23 18:08:36 +0100

    Adds Support for CSS Modules

--- config/webpack/loadCss.js
+++ config/webpack/loadCss.js
@@ -6,20 +6,20 @@ const compileScss = require('./compileScss.js')
 
 module.exports = (config) => {
     config = compileScss(config)
-
-    for (let rule of config.module.rules) {
-        if (!rule.test) {
-            continue
-        }
-
-        let tests = Array.isArray(rule.test) ? rule.test : [rule.test]
-        for (let regexp of tests) {
-            if (regexp.test('test.css') || regexp.test('test.scss')) {
-                rule.use.unshift({ loader: require.resolve('style-loader') })
-                break
-            }
-        }
-    }
-
+    //
+    //    for (let rule of config.module.rules) {
+    //        if (!rule.test) {
+    //            continue
+    //        }
+    //
+    //        let tests = Array.isArray(rule.test) ? rule.test : [rule.test]
+    //        for (let regexp of tests) {
+    //            if (regexp.test('test.css') || regexp.test('test.scss')) {
+    //                rule.use.unshift({ loader: require.resolve('style-loader') })
+    //                break
+    //            }
+    //        }
+    //    }
+    //
     return config
 }
