1ff320509d3c254669ba15235277445e7d9e46c6
Marcel Fahle <m.fahle@gmail.com>
2019-11-23 18:08:36 +0100

    Adds Support for CSS Modules

--- config/webpack/extractCss.js
+++ config/webpack/extractCss.js
@@ -7,32 +7,33 @@ const compileScss = require('./compileScss.js')
 module.exports = (config) => {
     config = compileScss(config)
 
-    for (let rule of config.module.rules) {
-        if (!rule.test) {
-            continue
-        }
+    // for (let rule of config.module.rules) {
+    //     if (!rule.test) {
+    //         continue
+    //     }
 
-        let tests = Array.isArray(rule.test) ? rule.test : [rule.test]
-        for (let regexp of tests) {
-            if (regexp.test('test.css') || regexp.test('test.scss')) {
-                rule.use.unshift({ loader: MiniCssExtractPlugin.loader })
-                break
-            }
-        }
-    }
+    //     let tests = Array.isArray(rule.test) ? rule.test : [rule.test]
+    //     for (let regexp of tests) {
+    //         if (regexp.test('test.css') || regexp.test('test.scss')) {
+    //             rule.use.unshift({ loader: MiniCssExtractPlugin.loader })
+    //             break
+    //         }
+    //     }
+    // }
 
-    return merge.smart(
-        config,
-        {
-            plugins: [
-                // Extract CSS code statically
-                new MiniCssExtractPlugin({
-                    // Options similar to the same options in
-                    // webpackOptions.output both options are optional
-                    filename: 'assets/css/[name].[hash:8].css',
-                    chunkFilename: 'assets/css/[name].[hash:8].css',
-                }),
-            ],
-        },
-    )
+    // return merge.smart(
+    //     config,
+    //     {
+    //         plugins: [
+    //             // Extract CSS code statically
+    //             new MiniCssExtractPlugin({
+    //                 // Options similar to the same options in
+    //                 // webpackOptions.output both options are optional
+    //                 filename: 'assets/css/[name].[hash:8].css',
+    //                 chunkFilename: 'assets/css/[name].[hash:8].css',
+    //             }),
+    //         ],
+    //     },
+    // )
+    return config;
 }
