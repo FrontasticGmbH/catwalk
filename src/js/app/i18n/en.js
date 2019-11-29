1fb9fa96019d38bb138840686530635cc0db7c34
chloe0592 <klaudija.ljevar@gmail.com>
2019-11-29 15:54:09 +0100

    translations newsletter, footer, product-view

--- src/js/app/i18n/en.js
+++ src/js/app/i18n/en.js
@@ -10,6 +10,32 @@ export default {
     },
     deleteItem: 'Delete Item',
     // Klaudija
+    exlusiveDiscounts: 'Exlusive discounts',
+    becomeFamily: 'Become part of our little family',
+    submit: 'Submit',
+    submitAgree: "By clicking 'Submit', you agree that we may process your information in accordance with our privacy policy.",
+    yourEmail: 'Your E-mail address',
+    contactFooter: 'Contact',
+    serviceFooter: '24 hour service',
+    writeUsFooter: 'Write to us',
+    paymentTermsFooter: 'Payment terms',
+    companyFooter: 'Company',
+    aboutUsFooter: 'About us',
+    partnershipsFooter: 'Partnerships',
+    careerFooter: 'Career',
+    pressFooter: 'Press',
+    serviceFooter: 'Service',
+    trackFooter: 'Track your orders',
+    faqFooter: 'FAQ',
+    deliveryFooter: 'Delivery',
+    returnFooter: 'Return',
+    jobsFooter: 'Jobs',
+    tcFooter: 'T&Cs',
+    imprintFooter: 'Imprint',
+    privacyFooter:'Privacy',
+    sizeProduct: 'Size',
+    inCartProduct: 'In the cart',
+
 
     // Sanja
 
