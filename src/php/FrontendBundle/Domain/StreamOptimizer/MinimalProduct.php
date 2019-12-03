f242ca5eec4fe6b28220866c73940b61bc94c32b
Kore Nordmann <mail@kore-nordmann.de>
2019-12-03 11:53:06 +0100

    Implemented minimal product example

---
+++ src/php/FrontendBundle/Domain/StreamOptimizer/MinimalProduct.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Frontastic\Catwalk\FrontendBundle\Domain\StreamOptimizer;
+
+use Frontastic\Catwalk\FrontendBundle\Domain\StreamOptimizer;
+use Frontastic\Catwalk\FrontendBundle\Domain\StreamContext;
+use Frontastic\Catwalk\FrontendBundle\Domain\Stream;
+
+use Frontastic\Common\ProductApiBundle\Domain\Product;
+use Frontastic\Common\ProductApiBundle\Domain\Variant;
+
+class MinimalProduct implements StreamOptimizer
+{
+    /**
+     * Attributes to keep in variant
+     *
+     * @var array
+     */
+    private $attributes = [];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function optimizeStreamData(Stream $stream, StreamContext $streamContext, $data)
+    {
+        if ($stream->type !== 'product-list') {
+            return $data;
+        }
+
+        foreach ($data->items as $index => $product) {
+            $data->items[$index] = new Product([
+                'productId' => $product->productId,
+                'slug' => $product->slug,
+                'name' => $product->name,
+                'variants' => [
+                    new Variant([
+                        'sku' => $product->variants[0]->sku,
+                        'price' => $product->variants[0]->price,
+                        'images' => $product->variants[0]->images,
+                        'attributes' => array_intersect_key(
+                            $product->variants[0]->attributes,
+                            array_flip($this->attributes)
+                        ),
+                    ])
+                ]
+            ]);
+        }
+
+        return $data;
+    }
+}
