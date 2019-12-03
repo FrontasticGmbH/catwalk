80dac2013a6de81f5bf0988304a8e7b6a5198110
Kore Nordmann <mail@kore-nordmann.de>
2019-12-03 14:52:17 +0100

    Reference docs in code

--- src/php/FrontendBundle/Domain/StreamOptimizer.php
+++ src/php/FrontendBundle/Domain/StreamOptimizer.php
@@ -2,6 +2,10 @@
 
 namespace Frontastic\Catwalk\FrontendBundle\Domain;
 
+/**
+ * Documentation about using this interface can be found here:
+ * https://frontastic.io/docs/catwalk/performance/20_stream_optimization/
+ */
 interface StreamOptimizer
 {
     /**
