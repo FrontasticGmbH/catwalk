83e446c2ad7da7e1867830fbeb57d53f50869afa
Kore Nordmann <mail@kore-nordmann.de>
2020-02-11 08:39:20 +0100

    Updated package compilation rule for new package names

--- config/ie11packages.js
+++ config/ie11packages.js
@@ -3,6 +3,6 @@ way of describing it and is just here for convenience.
 This should be refactored ASAP. *marcel
 */
 
-const ie11regex = /node_modules\/(?!(frontastic-catwalk|frontastic-common|ansi-styles|chalk|diff-dom|react-intl|domino\/lib\/NodeList.es6.js|react-dev-utils)).*/
+const ie11regex = /node_modules\/(?!(@frontastic\/catwalk|@frontastic\/common|@frontastic\/theme-boost|ansi-styles|chalk|diff-dom|react-intl|domino\/lib\/NodeList.es6.js|react-dev-utils)).*/
 
 module.exports = ie11regex
