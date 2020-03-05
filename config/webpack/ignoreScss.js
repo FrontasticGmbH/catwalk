236d2f22237a91387d2c592ca094cfa9633bcbb5
Kore Nordmann <mail@kore-nordmann.de>
2020-03-05 08:25:30 +0100

    Ensure CSS modules do not break styling during SSR
    We still extract (dummy) CSS during SSR so we get the proper class names
    out of the CSS modules. Otherwise we are left with `undefined` class
    names in the HTML tree and the React hydration does not always fix them
    which causes an unclean first rendered version of the page.

--- config/webpack/ignoreScss.js
+++ config/webpack/ignoreScss.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
+const autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (config, PRODUCTION, SERVER) => {
     return merge.smart(
@@ -7,13 +9,48 @@ module.exports = (config, PRODUCTION, SERVER) => {
         {
             plugins: [
                 new webpack.IgnorePlugin(/^\.css$/, /\.scss$/),
+                new MiniCssExtractPlugin({
+                    filename: '../build/css/[name].[hash:8].css',
+                    chunkFilename: '../build/css/[name].[hash:8].css',
+                }),
             ],
             module: {
                 rules: [
                     {
+                        test: /\.module\.s?css$/,
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            {
+                                // Translates CSS into CommonJS
+                                loader: require.resolve('css-loader'),
+                                options: { modules: true, importLoaders: 1 },
+                            },
+                            {
+                                loader: require.resolve('postcss-loader'),
+                                options: {
+                                    ident: 'postcss',
+                                    plugins: () => {
+                                        return [require('postcss-flexbugs-fixes'), require('tailwindcss'), autoprefixer()]
+                                    },
+                                },
+                            },
+                            {
+                                // Resolve relative url() paths
+                                loader: require.resolve('resolve-url-loader'),
+                            },
+                            {
+                                // Compiles Sass to CSS
+                                loader: require.resolve('sass-loader'),
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                        ],
+                    },
+                    {
                         test: [
-                            /\.css$/,
-                            /\.scss$/,
+                            /(?<!\.module)\.css$/,
+                            /(?<!\.module)\.scss$/,
                             /\.less/,
                         ],
                         use: [
