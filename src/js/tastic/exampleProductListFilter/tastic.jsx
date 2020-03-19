b9fa7951e1b6eecf2af26b2362872833583d6691
Tobias Schlitt <toby@php.net>
2020-03-19 08:30:50 +0100

    Implemented exemplary selection/deselection of term facet filter.

--- src/js/tastic/exampleProductListFilter/tastic.jsx
+++ src/js/tastic/exampleProductListFilter/tastic.jsx
@@ -14,7 +14,8 @@ import urlHandlerConnector from '../../app/connector/urlHandler'
 class ExampleProductListFilterTastic extends Component {
 
     render () {
-        let productList = this.props.rawData.stream[this.props.tastic.configuration.stream]
+        const productList = this.props.data.stream
+
         if (!productList || !productList.facets || productList.facets.length === 0) {
             // eslint-disable-next-line no-console
             console.warn('Product list does not contain facets. Won\'t display filter tastic.')
@@ -27,41 +28,82 @@ class ExampleProductListFilterTastic extends Component {
             return null
         }
 
+        const colorFacet = _.find(productList.facets, { handle: 'variants.attributes.color' })
+        if (!colorFacet) {
+            // eslint-disable-next-line no-console
+            console.error('Missing facet variants.attributes.color from the example dataset. Please activate it in Backstage!')
+            return null
+        }
+
+        const sizeFacet = _.find(productList.facets, { handle: 'variants.attributes.size' })
+        if (!sizeFacet) {
+            // eslint-disable-next-line no-console
+            console.error('Missing facet variants.attributes.size from the example dataset. Please activate it in Backstage!')
+            return null
+        }
+
+        const streamId = this.props.tastic.configuration.stream
+        console.log(streamId)
+
         return (<Fragment>
-            <p><strong>I am the selector!!!!1elf</strong></p>
+            <div style={{ width: '50%', 'float': 'left' }}>
+            <h1>Color Filter:</h1>
+            <ul>
+                {colorFacet.terms.map((term) => {
+                    return this.renderTerm(term, (event) => {
+                        event.preventDefault()
+                        this.toggleFacetTerm(streamId, colorFacet, term)
+                    })
+                })}
+            </ul>
+            </div>
+            <div style={{ width: '50%', 'float': 'left' }}>
+            <h1>Size Filter:</h1>
+            <ul>
+                {sizeFacet.terms.map((term) => {
+                    return this.renderTerm(term, (event) => {
+                        event.preventDefault()
+                        this.toggleFacetTerm(streamId, sizeFacet, term)
+                    })
+                })}
+            </ul>
+            </div>
         </Fragment>)
     }
 
-    getFacetValues = (streamId) => {
-        if (!this.props.urlHandler) {
-            return {}
-        }
-        return this.props.urlHandler.parameterReader(streamId).getParameters().facets || {}
+    renderTerm = (term, toggleCallback) => {
+        return (<li>
+            <a href='#' onClick={toggleCallback}>{term.value}</a>
+            ({term.count}) {term.selected ? '+' : ''}
+        </li>)
     }
 
-    selectFacetValue = (facet, value) => {
+    toggleFacetTerm = (streamId, facet, term) => {
         const parameters = this.props.urlHandler.deriveParameters((urlState) => {
-            urlState.getStream(this.props.tastic.configuration.stream).setFilter(facet.handle, value)
+            const streamState = urlState.getStream(streamId)
+            let filterTerms = this.getSelectedTerms(streamState, facet)
+
+            if (filterTerms.includes(term.handle)) {
+                filterTerms = filterTerms.splice(filterTerms.indexOf(term.handle), 1)
+            } else {
+                filterTerms.push(term.handle)
+            }
+
+            streamState.setFilter(
+                facet.handle,
+                _.isEmpty(filterTerms) ? null : { terms: filterTerms })
         })
 
         app.getRouter().push(this.props.route.route, parameters)
     }
 
-    removeFacetValue = (facet) => {
-        const parameters = this.props.urlHandler.deriveParameters((urlState) => {
-            urlState.getStream(this.props.tastic.configuration.stream).removeFilter(facet.handle)
-        })
-
-        app.getRouter().push(this.props.route.route, parameters)
-    }
-
-    filterCategoryTerms = (terms) => {
-        return _.uniqBy(terms, 'name')
+    getSelectedTerms = (streamState, facet) => {
+        return ((streamState.getParameters().facets || {})[facet.handle] || {}).terms || []
     }
 }
 
 ExampleProductListFilterTastic.propTypes = {
-    rawData: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
     tastic: PropTypes.object.isRequired,
 
     // From connector
