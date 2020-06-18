edcda021f749ce8086dfb3f19ae27bc87f55efd6
Carlos Mourullo <carlos@frontastic.cloud>
2020-06-18 12:32:00 +0100

    feat: custom fields
    Removed non mapped CommerceTools fields

--- src/php/FrontendBundle/Domain/Commercetools/RawDataService.php
+++ src/php/FrontendBundle/Domain/Commercetools/RawDataService.php
@@ -6,94 +6,15 @@ use Frontastic\Common\CoreBundle\Domain\ApiDataObject;
 
 class RawDataService
 {
-    const COMMERCETOOLS_ACCOUNT_FIELDS = [
-        'vatId' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setVatId',
-        ],
-        'customerNumber' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setCustomerNumber',
-        ],
-    ];
+    const COMMERCETOOLS_ACCOUNT_FIELDS = [];
 
-    const COMMERCETOOLS_ADDRESS_FIELDS = [
-        'key' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'title' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'region' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'state' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'company' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'department' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'building' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'apartment' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'pOBox' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'mobile' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'email' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-        'fax' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => null,
-        ],
-    ];
+    const COMMERCETOOLS_ADDRESS_FIELDS = [];
 
-    const COMMERCETOOLS_CART_FIELDS = [
-        'country' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setCountry',
-        ],
-        'customerGroup' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setCustomerGroup',
-        ],
-        'taxMode' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'changeTaxMode',
-        ],
-        'taxRoundingMode' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'changeTaxRoundingMode',
-        ],
-        'shippingRateInput' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setShippingRateInput',
-        ],
-        'taxCalculationMode' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'changeTaxCalculationMode',
-        ],
-        'deleteDaysAfterLastModification' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setDeleteDaysAfterLastModification',
-        ],
-        'itemShippingAddresses' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'updateItemShippingAddress',
-        ],
-    ];
+    const COMMERCETOOLS_CART_FIELDS = [];
 
     const COMMERCETOOLS_LINE_ITEM_FIELDS = [];
 
-    const COMMERCETOOLS_PAYMENT_FIELDS = [
-        'customer' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setCustomer',
-        ],
-        'anonymousId' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setAnonymousId',
-        ],
-        'interfaceId' => [
-            self::COMMERCETOOLS_ACTION_NAME_KEY => 'setInterfaceId',
-        ],
-    ];
+    const COMMERCETOOLS_PAYMENT_FIELDS = [];
 
     const COMMERCETOOLS_ACTION_NAME_KEY = 'actionName';
 
