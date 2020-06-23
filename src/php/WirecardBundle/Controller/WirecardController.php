ec227f30b373bfece236c14b4624466b7861ded7
Florian Sowade <f.sowade@suora.com>
2020-06-23 18:04:45 +0200

    Start to work on Wirecard payment integration

--- src/php/WirecardBundle/Controller/WirecardController.php
+++ src/php/WirecardBundle/Controller/WirecardController.php
@@ -2,15 +2,31 @@
 
 namespace Frontastic\Catwalk\WirecardBundle\Controller;
 
+use Frontastic\Catwalk\ApiCoreBundle\Domain\Context;
+use Frontastic\Catwalk\WirecardBundle\Domain\RegisterPaymentCommand;
 use Frontastic\Catwalk\WirecardBundle\Domain\WirecardService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Frontastic\Common\CartApiBundle\Controller\CartController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
 
-class WirecardController extends AbstractController
+class WirecardController extends CartController
 {
-    public function testAction()
+    public function registerPaymentAction(string $type, Context $context, Request $request)
     {
         $wirecardService = $this->get(WirecardService::class);
 
-        return ['success' => $wirecardService->test()];
+        $body = $this->getJsonContent($request);
+        if (!is_int($body['amount'] ?? null)) {
+            throw new PreconditionFailedHttpException('missing amount in JSON body');
+        }
+
+        $cart = $this->getCart($context, $request);
+
+        return $wirecardService->registerPayment(new RegisterPaymentCommand([
+            'paymentId' => $wirecardService->buildPaymentId($cart),
+            'cart' => $cart,
+            'type' => $type,
+            'amount' => $body['amount'],
+        ]));
     }
 }
--- test/php/WirecardBundle/Domain/WirecardServiceIntegrationTest.php
+++ test/php/WirecardBundle/Domain/WirecardServiceIntegrationTest.php
@@ -3,6 +3,7 @@
 namespace Frontastic\Catwalk\WirecardBundle\Domain;
 
 use Frontastic\Catwalk\IntegrationTest;
+use Frontastic\Common\CartApiBundle\Domain\Cart;
 
 class WirecardServiceIntegrationTest extends IntegrationTest
 {
@@ -17,8 +18,65 @@ class WirecardServiceIntegrationTest extends IntegrationTest
         $this->wirecardService = $this->getContainer()->get(WirecardService::class);
     }
 
-    public function testTestMethod()
+    public function testPaymentIdIsUnique()
     {
-        $this->assertTrue($this->wirecardService->test());
+        $cart = new Cart([
+            'cartId' => 'myCartId',
+        ]);
+
+        $paymentId1 = $this->wirecardService->buildPaymentId($cart);
+        $paymentId2 = $this->wirecardService->buildPaymentId($cart);
+
+        $this->assertNotEquals($paymentId1, $paymentId2);
+    }
+
+    public function testRegisterPaymentWithWrongAmountThrowsException()
+    {
+        $command = new RegisterPaymentCommand([
+            'paymentId' => uniqid('', true),
+            'amount' => 100,
+            'cart' => new Cart([
+                'cartId' => '456',
+                'sum' => 90,
+            ]),
+            'type' => RegisterPaymentCommand::TYPE_CREDITCARD,
+        ]);
+
+        $this->expectException(\DomainException::class);
+        $this->wirecardService->registerPayment($command);
+    }
+
+    public function testRegisterPaymentWithUnknownTypeThrowsException()
+    {
+        $command = new RegisterPaymentCommand([
+            'paymentId' => uniqid('', true),
+            'amount' => 100,
+            'cart' => new Cart([
+                'cartId' => '456',
+                'sum' => 100,
+            ]),
+            'type' => 'unknown_payment_type',
+        ]);
+
+        $this->expectException(\DomainException::class);
+        $this->wirecardService->registerPayment($command);
+    }
+
+    public function testRegisterCreditcardPayment()
+    {
+        $command = new RegisterPaymentCommand([
+            'paymentId' => uniqid('', true),
+            'amount' => 100,
+            'cart' => new Cart([
+                'cartId' => '456',
+                'sum' => 100,
+            ]),
+            'type' => RegisterPaymentCommand::TYPE_CREDITCARD,
+        ]);
+
+        $result = $this->wirecardService->registerPayment($command);
+
+        $this->assertSame(RegisterPaymentResult::PAYMENT_MODE_SEAMLESS, $result->paymentMode);
+        $this->assertStringStartsWith('https://', $result->paymentUrl);
     }
 }
