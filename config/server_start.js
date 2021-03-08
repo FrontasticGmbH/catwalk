0a6849bb29b92e1073f68b4921ae0355617d4de0
Sven Koschnicke <s.koschnicke@gfxpro.com>
2021-03-08 09:30:36 +0100

    fix: improve SSR start scripts

--- config/server_start.js
+++ config/server_start.js
@@ -1,17 +1,18 @@
-// touch build/assets/js/devServer.js ;
 const fs = require('fs');
+const { spawnSync } = require('child_process');
+const process = require('process');
+
+// touch build/assets/js/devServer.js ;
 const path = 'build/assets/js/devServer.js'
 try {
     fs.writeFileSync(path, '', { flag: 'wx' }); // throws error if file exists
     console.log("touched "+path);
 } catch(err) {
-    // ignore, we only want the file to exists
+    // ignore, we only want the file to exist
 }
 
 // NODE_ICU_DATA=$(node-full-icu-path)
 // nodemon -e js --watch build/assets/js/devServer.js --legacy-watch build/assets/js/devServer.js build/assets/js/devServer.js
-const { spawn, exec } = require('child_process');
-const process = require('process');
 
 /*
   node-full-icu-path seems not even installed (package full-icu)
@@ -23,14 +24,36 @@ exec('node-full-icu-path', function(error, stdout, stderr) {
 });
 */
 
-console.log('starting server')
-
+const onWindows = process.platform == 'win32'
 // is required to avoid ENOENT error on windows, see
 // https://stackoverflow.com/a/54515183/390808
-const shell = process.platform == 'win32'
+const shell = onWindows
 
-spawn(
+const ac = new AbortController();
+
+function handleStop() {
+    console.log('Received signal. Stopping child.');
+    ac.abort();
+    process.exit(1);
+}
+
+// TODO: windows seems to  make problems when listening to these events, only do on non-windows
+if (!onWindows) {
+    process.once('SIGINT', handleStop)
+    process.once('SIGTERM', handleStop)
+    process.once('SIGBREAK', handleStop)
+}
+
+console.log('starting server')
+console.log('sync!')
+
+spawnSync(
     'nodemon',
     ['-e', 'js', '--watch', path, '--legacy-watch', path, path],
-    {stdio: 'inherit', shell: shell}
+    {
+        stdio: 'inherit',
+        shell: shell,
+        detached: false,
+        abort: ac
+    }
 );
--- config/server_watch.js
+++ config/server_watch.js
@@ -1,15 +1,34 @@
-// NODE_ENV=development ../node_modules/.bin/webpack-cli --watch --config ../node_modules/@frontastic/catwalk/config/webpack.server.development.js
-const { spawn, exec } = require('child_process');
+const { spawnSync } = require('child_process');
 const process = require('process');
 
 // is required to avoid ENOENT error on windows, see
 // https://stackoverflow.com/a/54515183/390808
 const shell = process.platform == 'win32'
 
+const ac = new AbortController();
+
+function handleStop() {
+    console.log('Received signal. Stopping child.');
+    ac.abort();
+    process.exit(0);
+}
+
+process.on('SIGINT', handleStop)
+process.on('SIGTERM', handleStop)
+process.on('SIGBREAK', handleStop)
+
 console.log('starting ssr compile')
+
 process.env.NODE_ENV = 'development'
-spawn(
+
+spawnSync(
     'webpack-cli',
     ['--watch', '--config', '../node_modules/@frontastic/catwalk/config/webpack.server.development.js'],
-    {stdio: 'inherit', shell: shell, env: process.env}
+    {
+        stdio: 'inherit',
+        shell: shell,
+        env: process.env,
+        detached: false,
+        abort: ac
+    }
 );
