89e539c365eb909f18527da9ffa9e055e9e371c9
James Morrison <james@frontastic.cloud>
2021-05-26 20:56:15 +0100

    chore(catwalk): pre-render async component height by device type

--- src/js/component/asyncComponent.jsx
+++ src/js/component/asyncComponent.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+
+const mapStateToProps = (globalState) => {
+    return {
+        deviceType: globalState.renderContext.deviceType
+    }
+}
 
 const asyncComponent = (component) => {
-    return class extends React.Component {
+    return connect(mapStateToProps)(class extends React.Component {
         state = {
             component: null,
         }
 
-        componentDidMount () {
+        componentDidMount() {
             component.import()
                 .then(component => {
                     this.setState({ component: component.default })
                 })
         }
 
-        render () {
+        render() {
             const InnerComponent = this.state.component
 
             if (!InnerComponent) {
-                // @TODO: Is there a sensible way to pre-render height device
-                // specific?
                 return (
                     <div
                         className='c-asyc-component'
-                        style={{ height: component.height.desktop + 'px' }}
+                        style={{ height: component.height[this.props.deviceType] || component.height.mobile + 'px' }}
                     />
                 )
             }
 
             return <InnerComponent {...this.props} />
         }
-    }
+    })
 }
 
 export default asyncComponent
