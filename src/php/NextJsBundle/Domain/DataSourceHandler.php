5aadffb74df73c4d588095fa3d32b669735d0d1c
Tobias Schlitt <toby@frontastic.cloud>
2021-07-27 08:53:05 +0200

    feat(FP-859-nextjs-apis): Chore: Remove orphan docs.

--- src/php/NextJsBundle/Domain/DataSourceHandler.php
+++ src/php/NextJsBundle/Domain/DataSourceHandler.php
@@ -2,18 +2,11 @@
 
 namespace Frontastic\Catwalk\NextJsBundle\Domain;
 
-use Frontastic\Catwalk\FrontendBundle\Domain\Stream;
-
 /**
  * This interface is to model the API used for data source implementations in Frontastic NextJS.
  */
 interface DataSourceHandler
 {
-    /**
-     * @param DataSourceConfiguration $configuration
-     * @param DataSourceContext $context
-     * @return DataSourceResult
-     */
     public function provideData(
         DataSourceConfiguration $configuration,
         DataSourceContext $context
