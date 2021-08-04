4dff5a163122c98c84dca3de2f6a55825dd47771
jim <jim@frontastic.cloud>
2021-08-04 18:02:00 +0300

    chore: Updated the CustomDataSource migration script to use the right character set and collation

--- src/schema/2108040905_custom_data_source.sql
+++ src/schema/2108040905_custom_data_source.sql
@@ -7,11 +7,13 @@
     c_icon VARCHAR(255) NOT NULL,
     c_category VARCHAR(255) NOT NULL,
     c_configuration_schema LONGTEXT NOT NULL COMMENT '(DC2Type:object)',
-    c_environment varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+    c_environment VARCHAR(255) DEFAULT NULL,
     c_meta_data LONGTEXT NOT NULL COMMENT '(DC2Type:object)',
     c_is_active TINYINT(1) NOT NULL,
     c_is_deleted TINYINT(1) NOT NULL,
     INDEX IDX_948FBA091D79BD3 (c_id),
     INDEX IDX_948FBA0431551E0 (c_sequence),
     PRIMARY KEY(c_id)
-) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci
+  ENGINE = InnoDB;
