c35d6f29b9a828503d22b288e9a8c8f4d9ead5ab
jim <jim@frontastic.cloud>
2021-08-04 11:27:37 +0300

    fix: CustomDataSource schema fixes

--- src/schema/2108040905_custom_data_source.sql
+++ src/schema/2108040905_custom_data_source.sql
@@ -6,12 +6,12 @@
     c_description VARCHAR(255) NOT NULL,
     c_icon VARCHAR(255) NOT NULL,
     c_category VARCHAR(255) NOT NULL,
-    c_configuration_schema varchar(255) longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    c_configuration_schema LONGTEXT NOT NULL COMMENT '(DC2Type:object)',
     c_environment varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
     c_meta_data LONGTEXT NOT NULL COMMENT '(DC2Type:object)',
     c_is_active TINYINT(1) NOT NULL,
     c_is_deleted TINYINT(1) NOT NULL,
-    INDEX IDX_56B9BA28A6096BE1 (c_id),
-    INDEX IDX_56B9BA28A53C9AA4 (c_sequence),
+    INDEX IDX_948FBA091D79BD3 (c_id),
+    INDEX IDX_948FBA0431551E0 (c_sequence),
     PRIMARY KEY(c_id)
 ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB;
