baa42263c86d7dc20e8b36e0a6fab23c98eb6d9f
Tobias Schlitt <toby@frontastic.cloud>
2021-08-13 10:07:23 +0200

    feat: Implement possibility to override actions to old controllers.

--- src/php/NextJsBundle/Controller/ActionController.php
+++ src/php/NextJsBundle/Controller/ActionController.php
@@ -8,18 +8,27 @@ use Frontastic\Catwalk\NextJsBundle\Domain\Api\Response;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class ActionController
 {
     private HooksService $hooksService;
+    private HttpKernelInterface $httpKernel;
+    private string $rootDir;
 
-    public function __construct(HooksService $hooksService)
+    public function __construct(HooksService $hooksService, HttpKernelInterface $httpKernel, string $rootDir)
     {
         $this->hooksService = $hooksService;
+        $this->httpKernel = $httpKernel;
+        $this->rootDir = $rootDir;
     }
 
     public function indexAction(string $namespace, string $action, SymfonyRequest $request)
     {
+        if ($this->hasOverride($namespace, $action)) {
+            return $this->performOverrideForward($namespace, $action, $request);
+        }
+
         $hookName = sprintf('action-%s-%s', $namespace, $action);
         if (!$this->hooksService->knowsHook($hookName)) {
             throw new BadRequestHttpException('Unknown action');
@@ -51,5 +60,33 @@ class ActionController
         }
 
         return $response;
+    }
+
+    private function performOverrideForward(string $namespace, string $action, SymfonyRequest $request): SymfonyResponse
+    {
+        $overrides = $this->getOverrides();
+        $controller = $overrides[$namespace][$action];
+
+        $subRequest = $request->duplicate(null, null, ['_controller' => $controller]);
+
+        return $this->httpKernel->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+    }
+
+    private function hasOverride(string $namespace, string $action): bool
+    {
+        $overrides = $this->getOverrides();
+
+        return (isset($overrides[$namespace]) && isset($overrides[$namespace][$action]));
+    }
+
+    private function getOverrides(): array
+    {
+        $overrideFile = sprintf('%s/config/action_override.json', $this->rootDir);
+
+        if (!file_exists($overrideFile)) {
+            return [];
+        }
+
+        return json_decode(file_get_contents($overrideFile), true);
     }
 }
