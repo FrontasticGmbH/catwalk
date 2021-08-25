3b8f81b756a02589b51d86f547e2273608002459
Sven Koschnicke <s.koschnicke@gfxpro.com>
2021-08-25 14:39:57 +0200

    feat(catwalk): add another type annotation to nextjs api

--- src/php/NextJsBundle/Domain/Api/Response.php
+++ src/php/NextJsBundle/Domain/Api/Response.php
@@ -5,13 +5,21 @@ namespace Frontastic\Catwalk\NextJsBundle\Domain\Api;
 /**
  * Response structure as used by Express.js version 4.x + Frontastic additions.
  * {@see https://expressjs.com/en/api.html#res}
+ * @type
  */
 class Response
 {
+
+    /**
+     * @var string
+     */
     public string $statusCode;
 
     // public string $statusMessage;
 
+    /**
+     * @var string
+     */
     public $body;
 
     // ... more Express.js/Node.js fields
