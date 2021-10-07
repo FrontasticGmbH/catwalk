1c174c6c9c98478a42c113e775c187e9be0ad514
jim <jim@frontastic.cloud>
2021-10-07 14:16:32 +0300

    fix: phpstan has an issue with php-jwt

--- src/php/NextJsBundle/Domain/FrontasticJWTSessionService.php
+++ src/php/NextJsBundle/Domain/FrontasticJWTSessionService.php
@@ -12,11 +12,13 @@ class FrontasticJWTSessionService
 
     public function decodeAndValidateToken(string $sessionData): ?array
     {
+        /** @phpstan-ignore-next-line */
         return (array) JWT::decode($sessionData, self::SALT, ['HS256']);
     }
 
     public function encodeData($cookieData): string
     {
+        /** @phpstan-ignore-next-line */
         $token = JWT::encode([$cookieData], self::SALT, 'HS256');
         return $token;
     }
