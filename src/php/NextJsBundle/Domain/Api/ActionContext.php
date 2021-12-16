b7f10287f1f87c605268e828245255db911cb406
Tobias Schlitt <toby@frontastic.cloud>
2021-12-16 14:06:33 +0100

    fix: Make ActionContext a proper data object.

--- src/php/NextJsBundle/Domain/Api/ActionContext.php
+++ src/php/NextJsBundle/Domain/Api/ActionContext.php
@@ -2,20 +2,17 @@
 
 namespace Frontastic\Catwalk\NextJsBundle\Domain\Api;
 
+use Kore\DataObject\DataObject;
+
 /**
  * This class only represents the information available, the TypeScript API should provide a mechanism to request each
  * of these fields and we only submit those which have been requested.
  * @type
  */
-class ActionContext
+class ActionContext extends DataObject
 {
     /**
      * @var Context
      */
     public ?Context $frontasticContext = null;
-
-    public function __construct(?Context $context)
-    {
-        $this->frontasticContext = $context;
-    }
 }
