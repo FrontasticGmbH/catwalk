86230a8efba064f9a8d7fc97b636a7130a98eb84
Sven Koschnicke <s.koschnicke@gfxpro.com>
2022-01-14 15:49:46 +0100

    feat(api-hub): allow testing of DataSources by directly calling a HTTP endpoint
    The endpoint is /frontastic/test/<identifier> where <identifier> is the
    identifier of the DataSource. Should be a POST request with application/json
    Content-Type and the DataSourceConfiguration.configuration data as JSON payload.
    
    Example:
    
      http POST http://swiss-demo.frontastic.io.local/frontastic/test/example/star-wars-movie movieId="ZmlsbXM6NQ=="
      or
      curl -s -H "Content-Type: application/json" -XPOST http://swiss-demo.frontastic.io.local/frontastic/test/example/star-wars-movie --data '{"movieId":"ZmlsbXM6NQ=="}'
    
    (you need a sandbox and a running Frontastic CLI for that)

---
+++ src/php/NextJsBundle/Controller/TestController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Frontastic\Catwalk\NextJsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+use Frontastic\Catwalk\ApiCoreBundle\Domain\Hooks\HooksService;
+use Frontastic\Catwalk\ApiCoreBundle\Domain\Context;
+use Frontastic\Catwalk\NextJsBundle\Domain\Api\DataSourceContext;
+use Frontastic\Catwalk\NextJsBundle\Domain\Api\DataSourceConfiguration;
+use Frontastic\Catwalk\NextJsBundle\Domain\Api\PageFolder;
+use Frontastic\Catwalk\NextJsBundle\Domain\Api\Page;
+use Frontastic\Catwalk\NextJsBundle\Domain\FromFrontasticReactMapper;
+use Frontastic\Catwalk\NextJsBundle\Domain\RequestService;
+
+class TestController
+{
+    private HooksService $hooksService;
+    private RequestService $requestService;
+    private FromFrontasticReactMapper $mapper;
+
+    public function __construct(HooksService $hooksService, RequestService $requestService, FromFrontasticReactMapper $mapper)
+    {
+        $this->hooksService = $hooksService;
+        $this->requestService = $requestService;
+        $this->mapper = $mapper;
+    }
+
+    public function dataSourceAction(string $identifier, SymfonyRequest $request, Context $context)
+    {
+        if ($request->headers->get('Content-Type') != 'application/json') {
+            return $this->unsupportedContentType();
+        }
+
+        $requestContentJson = json_decode($request->getContent(), true);
+
+        $hookName = 'data-source-' . str_replace('/', '-', $identifier); // copied from Typescript TODO: put that in a central place
+        return $this->hooksService->call(
+            $hookName,
+            [
+                new DataSourceConfiguration([
+                    'configuration' => $requestContentJson
+                ]),
+                new DataSourceContext([
+                    'frontasticContext' => $this->mapper->map($context),
+                    'pageFolder' => new PageFolder(
+                        ['pageFolderId' => 'somePageFolderId']
+                    ),
+                    'page' => new Page(
+                        ['pageId' => 'somePageId', 'state' => 'development']
+                    ),
+                    'usingTastics' => null,
+                    'request' => $this->requestService->createApiRequest($request)
+                ])
+            ]
+        );
+    }
+
+    private function unsupportedContentType(): SymfonyResponse
+    {
+        $response = new SymfonyResponse();
+        $response->setContent('Only JSON is supported (set Content-Type to \'application/json\')');
+        $response->setStatusCode(SymfonyResponse::HTTP_UNSUPPORTED_MEDIA_TYPE);
+        $response->headers->set('Content-Type', 'text/plain');
+        return $response;
+    }
+}
