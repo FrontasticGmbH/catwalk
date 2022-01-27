348c314187cd33a121fea632b68dc5965e96e266
hkovesdi <kovesdi.hunor99@gmail.com>
2022-01-27 09:30:00 +0100

    feat(FP-1328): Add unsupported language checks in FrontasticNextJsRedirectService

--- src/php/NextJsBundle/Domain/FrontasticNextJsRedirectService.php
+++ src/php/NextJsBundle/Domain/FrontasticNextJsRedirectService.php
@@ -7,7 +7,7 @@ use Frontastic\Catwalk\FrontendBundle\Domain\Redirect;
 use Frontastic\Catwalk\FrontendBundle\Domain\RedirectService;
 use Frontastic\Catwalk\FrontendBundle\Gateway\RedirectGateway;
 use Frontastic\Catwalk\NextJsBundle\Domain\Api\RedirectResponse;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 class FrontasticNextJsRedirectService extends RedirectService
@@ -28,11 +28,14 @@ class FrontasticNextJsRedirectService extends RedirectService
      * 1. There is a locale mismatch where a given path exists but does not match the current locale
      * 2. There is a redirect configured for the provided path
      * @param string $path
-     * @param ParameterBag $queryParameters Currently unused but will be appended to the redirect target in the future
+     * @param ParameterBag $queryParameters
      * @param Context $context
      */
-    public function getRedirectResponseForPath(string $path, ParameterBag $queryParameters, Context $context): ?RedirectResponse
-    {
+    public function getRedirectResponseForPath(
+        string $path,
+        ParameterBag $queryParameters,
+        Context $context
+    ): ?RedirectResponse {
         $redirect = $this->getLocaleMismatchRedirect($path, $context->locale, $context->project->languages);
 
         if ($redirect !== null) {
@@ -54,27 +57,29 @@ class FrontasticNextJsRedirectService extends RedirectService
      * A locale mismatch means that a given path exists but not for the given locale.
      * @param string $path The path of the sitebuilder page
      * @param string $currentLocale The locale which the given path should exist in
-     * @param array $locales The available locales in the project
+     * @param array $languages The available languages in the project
      */
-    public function getLocaleMismatchRedirect(string $path, string $currentLocale, array $locales): ?RedirectResponse
+    public function getLocaleMismatchRedirect(string $path, string $currentLocale, array $languages): ?RedirectResponse
     {
         // If there is no locale mismatch, return null
         if ($this->siteBuilderPageService->matchSiteBuilderPage($path, $currentLocale) !== null) {
             return null;
         }
 
-        foreach ($locales as $locale) {
-            $nodeId = $this->siteBuilderPageService->matchSiteBuilderPage($path, $locale);
+        foreach ($languages as $language) {
+            $nodeId = $this->siteBuilderPageService->matchSiteBuilderPage($path, $language);
 
             if ($nodeId !== null) {
                 $paths = $this->siteBuilderPageService->getPathsForSiteBuilderPage($nodeId);
 
-                return new RedirectResponse([
-                    'statusCode' => 301,
-                    'reason' => RedirectResponse::REASON_LOCALE_MISMATCH,
-                    'targetType' => RedirectResponse::TARGET_TYPE_PAGE_FOLDER,
-                    'target' => $paths[$locale]
-                ]);
+                if (array_key_exists($currentLocale, $paths)) {
+                    return new RedirectResponse([
+                        'statusCode' => 301,
+                        'reason' => RedirectResponse::REASON_LOCALE_MISMATCH,
+                        'targetType' => RedirectResponse::TARGET_TYPE_PAGE_FOLDER,
+                        'target' => $paths[$currentLocale]
+                    ]);
+                }
             }
         }
 
@@ -86,13 +91,19 @@ class FrontasticNextJsRedirectService extends RedirectService
      * @param Redirect $redirect
      * @param string $locale
      */
-    public function createResponseFromRedirectObject(Redirect $redirect, string $locale): RedirectResponse
+    public function createResponseFromRedirectObject(Redirect $redirect, string $locale): ?RedirectResponse
     {
         $target = $redirect->target;
         $targetType = $redirect->targetType;
 
         if ($redirect->targetType === Redirect::TARGET_TYPE_NODE) {
-            $target = $this->siteBuilderPageService->getPathsForSiteBuilderPage($redirect->target)[$locale];
+            $paths = $this->siteBuilderPageService->getPathsForSiteBuilderPage($redirect->target);
+
+            if (!array_key_exists($locale, $paths)) {
+                return null;
+            }
+
+            $target = $paths[$locale];
             $targetType = RedirectResponse::TARGET_TYPE_PAGE_FOLDER;
         }
 
