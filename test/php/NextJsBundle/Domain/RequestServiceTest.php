9fce8f6dc00d63cf87528954d1865cc038dee8b8
hkovesdi <kovesdi.hunor99@gmail.com>
2022-02-25 15:39:28 +0100

    misc(FP-1399): Final implementation of JWT decryption testing

--- test/php/NextJsBundle/Domain/RequestServiceTest.php
+++ test/php/NextJsBundle/Domain/RequestServiceTest.php
@@ -70,6 +70,13 @@ class RequestServiceTest extends TestCase
             'valid and encrypted' => [
                 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiMWlPRTBJR1VXV1FsY2xsMlNFQUcxRWNcL0dzVlFaXC9IXC9NXC9WZ0dyVT0iLCJub25jZSI6IjJ5aHB3MVhDdG9pcEdHbEcifQ.OhlZvQqPNKoh3rrIKXikD-h2c0OL0rkrT2ntEnlgzs0',
                 ['foo' => 'bar']
+            ],
+            'failing aes256 decryption' => [
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZUpFellxdmVwMHFpMWZNRDhcLyt2T0V2MFNvdWVvUzlQZXpnMnJXdz0iLCJub25jZSI6IlRTZ25WWktRVkd1RFp2QWYifQ.ndbNXh_C7da54Riw80oXtxsroL_SRye0WbXKFWRrvCc',
+                [
+                    "payload" =>  "eJEzYqvep0qi1fMD8/+vOEv0SoueoS9Pezg2rWw=",
+                    "nonce" =>  "TSgnVZKQVGuDZvAf"
+                ]
             ]
         ];
     }
@@ -80,19 +87,19 @@ class RequestServiceTest extends TestCase
         return [
             'simple 1 key 1 value' => [
                 ['foo' => 'bar'],
-                '',
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZUpFellxdmVwMHFpMWZNRDhcLyt2T0V2MFNvdWVvUzlQZXpnMnJXdz0iLCJub25jZSI6Ik1EQXdNREF3TURBd01EQXcifQ.QttFQsH_jeS_sugnJB-mNplG4st0e-SXc3Eirruo1PM',
             ],
             'empty' => [
                 [],
-                ''
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiV081dCtHV0RxSnlRWFwvMFVaSExmUVUxMyIsIm5vbmNlIjoiTURBd01EQXdNREF3TURBdyJ9.8tTqmMQivj_KvLHxsp9QQrrkZh3fI-z8lEZLALYk57k'
             ],
             'null value valid key' => [
                 ['foo' => null],
-                ''
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZUpFellxdmVwd2ExMk8xY1BvVWgxZlUwa2R5TTBnMkJwbkxRelE9PSIsIm5vbmNlIjoiTURBd01EQXdNREF3TURBdyJ9.Kx0aZyQWkWTh4sXXjV5dXbA4633nzQG2rfbwNNmDVfo'
             ],
             'null key null value' => [
                 [null => null],
-                ''
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZUpGM042cUo4UVM5czNtYUdSR3hPaHl2Zm5Ham01NXM1UT09Iiwibm9uY2UiOiJNREF3TURBd01EQXdNREF3In0.DK67U5SwMj9lKK9EPBeu5TE8HVr5lpOBc0pq7Jd8m3g'
             ],
             'complex nested object' => [
                 [
@@ -103,7 +110,7 @@ class RequestServiceTest extends TestCase
                     1 => 2,
                     3 => 'value'
                 ],
-                ''
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZUpFellxdmVweFBpMStsTzdaMHhKXC9GWjlVSXl2NHVyRkdtUGdwYlBZeERUNW9ZcHZYTzE0ZWczeUVreUp6V2FGeFIxSVhuZFhSanRzZHVcLzljV1ZOaTdpZXBwZGw5YW5SZz09Iiwibm9uY2UiOiJNREF3TURBd01EQXdNREF3In0.Qz_A3Uw7cEKlQZ5ETKiTrtH3iizDYP4IQ_-heH4flUM'
             ]
         ];
     }
@@ -122,9 +129,19 @@ class RequestServiceTest extends TestCase
      * @dataProvider JWTEncodeProvider
      */
     public function testEncodeJWTData($data, $expected)
-    {   
-        $result = $this->requestService->encodeJWTData($data);
+    {
+        $requestService = $this->getMockClass(
+            RequestService::class,
+            array('generateNonce')
+        );
 
-        // $this->assertSame($expected, $result);
+        $requestService = new $requestService(new NullLogger());
+        $requestService->expects($this->once())
+            ->method('generateNonce')
+            ->will($this->returnValue('000000000000'));
+
+        $result = $requestService->encodeJWTData($data);
+
+        $this->assertSame($expected, $result);
     }
 }
