f239dc24a2c0a621b832ceffc242fefc3a6b0e22
hkovesdi <kovesdi.hunor99@gmail.com>
2022-03-01 09:10:08 +0100

    fix(FP-1373): Remove CORS headers set by PHP to fix duplicate header errors

--- src/php/NextJsBundle/EventListener/CorsHandler.php
+++ src/php/NextJsBundle/EventListener/CorsHandler.php
@@ -8,11 +8,27 @@ use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class CorsHandler
 {
+    /**
+     * This function remove all CORS headers set by PHP in order
+     * to avoid duplicates which would lead to errors.
+     * Needed for backwards compatibility.
+     */
+    private function clearCORSHeaders()
+    {
+        header_remove('Access-Control-Allow-Origin');
+        header_remove('Access-Control-Allow-Methods');
+        header_remove('Access-Control-Allow-Headers');
+        header_remove('Access-Control-Expose-Headers');
+        header_remove('Access-Control-Allow-Credentials');
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
         if (!$event->isMasterRequest()) {
             return;
         }
+
+        $this->clearCORSHeaders();
 
         $method = $event->getRequest()->getRealMethod();
 
@@ -36,6 +52,8 @@ class CorsHandler
         if (!$event->isMasterRequest()) {
             return;
         }
+
+        $this->clearCORSHeaders();
 
         $headers = $event->getResponse()->headers;
         $origin = $event->getRequest()->headers->get('origin') ?? $event->getRequest()->getHost();
