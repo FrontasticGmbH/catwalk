7385f49e372e7a7b8b435763ab83c30b3f717ddd
Sven Koschnicke <s.koschnicke@gfxpro.com>
2022-05-06 08:19:42 +0200

    chore(api-hub): copy manual type files to source directory
    Because CI compares these two directories and fails if they differ.

---
+++ src/js/types/nextjs/api/exports.ts
@@ -0,0 +1,46 @@
+/*
+ * This file is the main entry point of the module and re-exports all relevant
+ * type. This way we can also include types from files in subdirectories,
+ * manually defined ones and rename types which don't get distinct names from
+ * the generator.
+ */
+export type {
+    ActionContext,
+    Configuration,
+    Context,
+    DataSourceConfiguration,
+    DataSourceContext,
+    DataSourceResult,
+    DynamicPageContext,
+    DynamicPageRedirectResult,
+    DynamicPageSuccessResult,
+    LayoutElement,
+    Page,
+    PageFolder,
+    Project,
+    Request,
+    Response,
+    Section,
+    Tastic,
+} from './index';
+
+export type { PageFolderTreeValue, PageFolderValue } from './tasticfieldvalue/index';
+export type { Configuration as LayoutElementConfiguration } from './layoutelement/index';
+export type { Configuration as TasticConfiguration } from './tastic/index';
+export type {
+    ComponentValueLinkReference,
+    ComponentValuePageFolderReference,
+    ComponentValueReference,
+} from './reference';
+export type { ComponentValueMedia } from './media';
+export type {
+    ComponentValueString,
+    ComponentValueText,
+    ComponentValueMarkdown,
+    ComponentValueNumber,
+    ComponentValueInteger,
+    ComponentValueDecimal,
+    ComponentValueEnum,
+    ComponentValueBoolean,
+    ComponentValueJson,
+} from './basicComponentValues';
