0740a08234232cead3f04afaf14abf3478b08373
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-07-28 12:35:25 +0100

    feat(FP-2242): Fix streamHandlerSupplier being called on classic, resulting in a null pointer

--- src/php/FrontendBundle/Domain/StreamService.php
+++ src/php/FrontendBundle/Domain/StreamService.php
@@ -377,7 +377,7 @@ class StreamService
         if (isset($this->streamHandlers[$stream->type])) {
             $streamHandler = $this->streamHandlers[$stream->type];
         } else {
-            if (getenv('is_frontastic_nextjs') === '1' || !isset($this->streamHandlerSupplier)) {
+            if (getenv('is_frontastic_nextjs') === '1' && isset($this->streamHandlerSupplier)) {
                 $streamHandler = $this->streamHandlerSupplier->get($stream->type);
             } else {
                 return Promise\rejection_for(
