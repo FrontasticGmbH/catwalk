58a218eeb3b5103ff7cfa221f97667376a94f9af
Sven Koschnicke <s.koschnicke@gfxpro.com>
2022-08-16 09:11:55 +0200

    fix(api-hub): fix typehint

--- src/php/ApiCoreBundle/Exception/ExtensionRunnerException.php
+++ src/php/ApiCoreBundle/Exception/ExtensionRunnerException.php
@@ -6,7 +6,7 @@ class ExtensionRunnerException extends \Exception
 {
     private array $context;
 
-    public function __construct(string $message, $code = 0, Throwable $previous = null, array $context = [])
+    public function __construct(string $message, $code = 0, \Throwable $previous = null, array $context = [])
     {
         $this->context = $context;
         parent::__construct($message, $code, $previous);
