02aebc1306170bfbea375db230af82cb7a692e9f
hkovesdi <kovesdi.hunor99@gmail.com>
2022-08-24 18:34:06 +0200

    feat(FP-2552): Create DataSourcePreviewPayloadElement, add previewPayload to DataSourceResult

---
+++ src/php/NextJsBundle/Domain/Api/DataSourcePreviewPayloadElement.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Frontastic\Catwalk\NextJsBundle\Domain\Api;
+
+use Kore\DataObject\DataObject;
+
+
+class DataSourcePreviewPayloadElement extends DataObject
+{
+    /**
+     * This will show up as the name of the element in the
+     * data source preview list in Studio.
+     * @required
+     */
+    public string $title;
+
+    /**
+     * This is the image URL that will be loaded when viewing
+     * the data source preview list in Studio.
+     */
+    public ?string $image;
+}
