2681cd055b85354eeb26efc8e3f39603bfe9db2b
Carlos Mourullo <carlosmourullo@gmail.com>
2022-10-20 09:50:30 +0200

    feat(FP-2874): used an object to store the frontend routes

--- src/schema/2210191010_frontend_routes.sql
+++ src/schema/2210191010_frontend_routes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE frontend_routes (
     fr_id INT NOT NULL,
-    fr_frontend_routes LONGTEXT NOT NULL COMMENT '(DC2Type:json_array)',
+    fr_frontend_routes LONGTEXT NOT NULL,
 
     PRIMARY KEY(fr_id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
