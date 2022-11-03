09dc34756ea5a937fd9e4682f36fbf7d29b5fc47
Tobias Schlitt <toby@frontastic.cloud>
2022-11-03 10:53:41 +0100

    fix(route-caching): Re-generate server-local route cache latest every 120 seconds.

---
+++ src/php/FrontendBundle/Routing/RoutingConfigCacheFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Frontastic\Catwalk\FrontendBundle\Routing;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Config\ConfigCacheFactoryInterface;
+
+class RoutingConfigCacheFactory implements ConfigCacheFactoryInterface
+{
+    private LoggerInterface $logger;
+    /**
+     * @var false
+     */
+    private bool $debug;
+
+    public function __construct(LoggerInterface $logger, $debug = false)
+    {
+        $this->logger = $logger;
+        $this->debug = $debug;
+    }
+
+    public function cache($file, $callback)
+    {
+        if (!\is_callable($callback)) {
+            throw new \InvalidArgumentException(sprintf('Invalid type for callback argument. Expected callable, but got "%s".', \gettype($callback)));
+        }
+
+        $this->logger->debug('Checking custom route cache');
+
+        $cache = new RoutingConfigCache($file, $this->logger, $this->debug);
+        if (!$cache->isFresh()) {
+            $callback($cache);
+        }
+
+        return $cache;
+    }
+}
