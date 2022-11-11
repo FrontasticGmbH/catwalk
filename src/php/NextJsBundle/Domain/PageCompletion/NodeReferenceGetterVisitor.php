f3eea8f9fec4f59e8b722d27ce52eca353cca7bc
Florian Sowade <f.sowade@suora.com>
2022-11-11 13:18:34 +0100

    Fetch referenced node IDs inside groups as well

--- src/php/NextJsBundle/Domain/PageCompletion/NodeReferenceGetterVisitor.php
+++ src/php/NextJsBundle/Domain/PageCompletion/NodeReferenceGetterVisitor.php
@@ -7,12 +7,26 @@ use Frontastic\Common\SpecificationBundle\Domain\Schema\FieldVisitor;
 
 class NodeReferenceGetterVisitor implements FieldVisitor
 {
+    /**
+     * @var string[]
+     */
+    private $referencedNodeIds = [];
+
+    /**
+     * @return string[]
+     */
+    public function getReferencedNodeIds(): array
+    {
+        return $this->referencedNodeIds;
+    }
+
     public function processField(FieldConfiguration $configuration, $value, array $fieldPath)
     {
         if ($configuration->getType() === 'reference' && isset($value['type']) && $value['type'] === 'node') {
-            $result = $value['target'];
+            $nodeId = $value['target'] ?? '';
+            if ($nodeId !== '') {
+                $this->referencedNodeIds[] = $nodeId;
+            }
         }
-
-        return $result ?? null;
     }
 }
