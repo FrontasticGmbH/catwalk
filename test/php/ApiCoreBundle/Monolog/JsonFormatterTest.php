16cb8f699aa18dfffc37b0977cc1af753b99a208
Joel Fickson <joelngozo@gmail.com>
2023-02-20 16:55:02 +0200

    feat: add tests for JSON Formatter[MonoLog]

--- test/php/ApiCoreBundle/Monolog/JsonFormatterTest.php
+++ test/php/ApiCoreBundle/Monolog/JsonFormatterTest.php
@@ -25,42 +25,40 @@ class JsonFormatterText extends TestCase
             $this->customerService,
             "staging"
         );
-
-
     }
 
     public function testFormatExceptionTraceProd()
     {
         $exception = new Exception("Something happened");
-        $formatException = self::getMethod("formatException");
+        $formatException = $this->getAppMethod("formatExceptionTrace", "prod");
 
-        $exceptionResponse = $formatException->invokeArgs($this->prodJsonFormatter, [$exception]);
+        $exceptionResponse = $formatException->invokeArgs($this->prodJsonFormatter,[$exception]);
 
         $this->assertIsArray($exceptionResponse);
         $this->assertArrayHasKey("file", $exceptionResponse);
         $this->assertArrayHasKey("line", $exceptionResponse);
     }
-//    public function testFormatExceptionTraceStaging()
-//    {
-//        $exception = new Exception("Something happened");
-//        $formatException = self::getMethod("formatException");
-//
-//        $exceptionResponse = $formatException->invokeArgs($this->stagingJsonFormatter, [$exception]);
-//
-//
-//        $this->assertIsArray($exceptionResponse);
-//        $this->assertArrayHasKey("file", $exceptionResponse);
-//        $this->assertArrayHasKey("line", $exceptionResponse);
-//        $this->assertArrayHasKey("trace", $exceptionResponse);
-//
-//    }
+    public function testFormatExceptionTraceStaging()
+    {
+        $exception = new Exception("Something happened");
+        $formatException = $this->getAppMethod("formatExceptionTrace", "staging");
 
-    protected static function getMethod($name) {
+        $exceptionResponse = $formatException->invokeArgs($this->stagingJsonFormatter, [$exception]);
 
-        $JsonClass = new JsonFormatter();
+        $this->assertIsArray($exceptionResponse);
+        $this->assertArrayHasKey("file", $exceptionResponse);
+        $this->assertArrayHasKey("line", $exceptionResponse);
+        $this->assertArrayHasKey("function", $exceptionResponse);
 
-        $class = new ReflectionClass("JsonFormatter.php");
-        return $class->getMethod($name);
+    }
+
+  public function getAppMethod($name, $env): ReflectionMethod
+  {
+        $reflection = new ReflectionClass("Frontastic\Catwalk\ApiCoreBundle\Monolog\JsonFormatter");
+        $reflection->newInstanceArgs([$this->customerService, $env]);
+        $method = $reflection->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
     }
 
 }
