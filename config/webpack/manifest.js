00ab3649397da228f57adac9d76cf16d412d8b81
Florian Sowade <f.sowade@suora.com>
2023-03-01 20:11:42 +0100

    chore: update webpack plugins

--- config/webpack/manifest.js
+++ config/webpack/manifest.js
@@ -1,4 +1,4 @@
-const ManifestPlugin = require('webpack-manifest-plugin')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 const merge = require('webpack-merge')
 
 module.exports = (config, PRODUCTION, SERVER) => {
@@ -8,7 +8,7 @@ module.exports = (config, PRODUCTION, SERVER) => {
                 // Generate a manifest file which contains a mapping of all asset filenames
                 // to their corresponding output file so that tools can pick it up without
                 // having to parse `index.html`.
-                new ManifestPlugin({
+                new WebpackManifestPlugin({
                     fileName: 'asset-manifest.json',
                 }),
             ]
