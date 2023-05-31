6c2231221a1945efbb814c667a2e5aecfc71be9d
Carlos Mourullo <carlosmourullo@gmail.com>
2023-05-31 11:25:18 +0200

    chore: fix CS and add missing headers

--- test/php/NextJsBundle/EventListener/CorsHandlerTest.php
+++ test/php/NextJsBundle/EventListener/CorsHandlerTest.php
@@ -29,7 +29,7 @@ class CorsHandlerTest extends TestCase
 
         $request->headers->set('Origin', 'https://frontastic.cloud');
         $request->headers->set('Access-Control-Request-Method', 'GET');
-        $request->headers->set('Access-Control-Request-Headers', 'Origin, Content-Type, Accept, Cookie, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version');
+        $request->headers->set('Access-Control-Request-Headers', 'Origin, Content-Type, Accept, Cookie, Frontastic-Currency, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version');
 
         return $request;
     }
@@ -84,7 +84,7 @@ class CorsHandlerTest extends TestCase
             $headers->get('Access-Control-Allow-Methods')
         );
         $this->assertEquals(
-            'Origin, Content-Type, Accept, Cookie, Frontastic-Session, X-Frontastic-Access-Token, Frontastic-Locale, Frontastic-Path, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
+            'Origin, Content-Type, Accept, Cookie, Frontastic-Session, X-Frontastic-Access-Token, Frontastic-Currency, Frontastic-Locale, Frontastic-Path, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
             $headers->get('Access-Control-Allow-Headers')
         );
         $this->assertEquals(
@@ -150,11 +150,11 @@ class CorsHandlerTest extends TestCase
             $headers->get('Access-Control-Allow-Methods')
         );
         $this->assertEquals(
-            'Origin, Content-Type, Accept, Cookie, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
+            'Origin, Content-Type, Accept, Cookie, Frontastic-Currency, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
             $headers->get('Access-Control-Allow-Headers')
         );
         $this->assertEquals(
-            '*, Authorization, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
+            '*, Authorization, Frontastic-Currency, Frontastic-Locale, Frontastic-Path, Frontastic-Session, X-Frontastic-Access-Token, coFE-Custom-Configuration, Commercetools-Frontend-Extension-Version',
             $headers->get('Access-Control-Expose-Headers')
         );
         $this->assertEquals(
