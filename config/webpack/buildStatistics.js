a31cb33214f6ba3f46e2b843fa91972d985951ee
Jan Krausenbaum <j.krausenbaum@suora.com>
2023-07-11 10:49:38 +0200

    feat(FP-2420): Remove unmaintained stats plugin
    This is causing deprecations with imminent breaking changes to webpack and would cause problems sooner than later

--- config/webpack/buildStatistics.js
+++ config/webpack/buildStatistics.js
@@ -1,5 +1,4 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const StatsPlugin = require('stats-webpack-plugin')
 const { merge } = require('webpack-merge')
 
 module.exports = (config, PRODUCTION, SERVER) => {
@@ -10,12 +9,6 @@ module.exports = (config, PRODUCTION, SERVER) => {
                 new BundleAnalyzerPlugin({
                     reportFilename: 'bundleSize.html',
                     analyzerMode: 'static',
-                }),
-
-                // Webpack dependency graph and other stats as JSON
-                new StatsPlugin('bundleStats.json', {
-                    chunkModules: true,
-                    exclude: [/node_modules[\\\/]react/],
                 }),
             ]
         },
