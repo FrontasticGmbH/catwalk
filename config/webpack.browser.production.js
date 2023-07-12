db130d2bad817f7aca2e15e6b4537f2ca70d756f
Jan Krausenbaum <j.krausenbaum@suora.com>
2023-07-12 12:30:41 +0200

    feat(FP-2420): Force vendor chunk name
    The vendor chunk was previously named vendors~main.[hash].[ext] which is relied upon by the later stage build steps including the entrypoints into the layout template.

--- config/webpack.browser.production.js
+++ config/webpack.browser.production.js
@@ -37,6 +37,7 @@ config.optimization = {
             defaultVendors: {
                 test: /\/node_modules\//,
                 priority: 5,
+                name: 'vendors~main',
             },
             default: {
                 minChunks: 2,
